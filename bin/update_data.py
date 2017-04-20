1c1ee8687cb3de57eb3ef0a0957163b5ebdf5fdc
Diego Tolentino <diegotolentino@gmail.com>
2017-04-20 18:00:46 -0300

    - Acho que scripts python maiores não devem ficar dentro de um arquivo python, pois é mais fácil manter/atualizar

---
+++ bin/update_data.py
@@ -0,0 +1,23 @@
+from datetime import timedelta, date
+
+import django
+
+django.setup()
+from gatheros_event.models import Event
+from gatheros_subscription.models import Lot
+
+i = 0
+ref_days = [-10, 10, 20, 25, 30]
+for event in Event.objects.all():
+    event.date_start = date.today() + timedelta(days=ref_days[i])
+    event.date_end = date.today() + timedelta(days=ref_days[i] + 1)
+    event.save()
+    i += 1
+
+i = 0
+lot_ref_days = [[0, 15], [16, 22], [23, 30]]
+for lot in Lot.objects.filter(event__pk=4):
+    lot.date_start = date.today() + timedelta(days=lot_ref_days[i][0])
+    lot.date_end = date.today() + timedelta(days=lot_ref_days[i][1])
+    lot.save()
+    i += 1
