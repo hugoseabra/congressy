98633c62b48df3b6b8e83cc4283db00f74e4e617
Hugo Seabra <hugoseabra19@gmail.com>
2017-04-24 15:52:37 -0300

    gatheros_event.Invitation: - verifica exclusão de membro de organização interna - invited_on com auto_now_add()

--- gatheros_event/models/member.py
+++ gatheros_event/models/member.py
@@ -1,4 +1,3 @@
-from django.db import IntegrityError
 from django.db import IntegrityError, models
 
 from . import Organization, Person
@@ -18,7 +17,7 @@ class Member(models.Model):
     group = models.CharField(max_length=20, choices=GROUP_CHOICES, verbose_name='grupo')
     created = models.DateTimeField(auto_now_add=True, verbose_name='criado em')
     created_by = models.PositiveIntegerField(verbose_name='criado por')  # ID do usuário
-    invited_on = models.DateTimeField(verbose_name='convidado em')
+    invited_on = models.DateTimeField(auto_now_add=True, verbose_name='convidado em')
     invitation_accepted = models.BooleanField(default=False, verbose_name='convite aceito')
     active = models.BooleanField(default=True, verbose_name='ativo')
 
@@ -34,13 +33,16 @@ class Member(models.Model):
         self.full_clean()
         super(Member, self).save(*args, **kwargs)
 
+    def delete(self, *args, **kwargs):
+        self._check_remove_if_internal_organization_member()
+        super(Member, self).delete(*args, **kwargs)
+
     def clean(self):
         self._configure_if_internal()
 
         if self.person.user is None:
             raise IntegrityError('Pessoas sem vínculo com usuários não podem ser participar de organizações')
 
-    # @TODO gerar exceção se org. interna apenas com ADMIN ou forçar ADMIN caso seja org. interno?
     def _configure_if_internal(self):
         if not self.organization.internal:
             return
@@ -48,4 +50,11 @@ class Member(models.Model):
         if self not in self.organization.members.all() and self.organization.members.count() > 0:
             raise IntegrityError('Organizações internas não podem ter membros')
 
-        self.group = self.ADMIN
+        if self.group == self.HELPER:
+            raise IntegrityError('Membro de organização interna não pode ser \'%s\'' % self.get_group_display())
+
+    def _check_remove_if_internal_organization_member(self):
+        if self.organization.internal is True:
+            raise IntegrityError(
+                'Impossível remover membro. Organização interna deve possuir um membro ADMIN principal.'
+            )
