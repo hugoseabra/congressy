0a1bc146f4c91ae7013e641407f8ee87fb5b1b0b
Hugo Seabra <hugoseabra19@gmail.com>
2017-05-01 21:58:14 -0300

    gatheros_subscription:
    - atualizando migration
    - atualizando reinstalltestdb.sh

--- bin/reinstalltestdb.sh
+++ bin/reinstalltestdb.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+export DJANGO_SETTINGS_MODULE=project.settings.dev
+
 export BASE_DIR=`python -c "
 import $DJANGO_SETTINGS_MODULE as settings
 print(settings.BASE_DIR)"`
@@ -21,8 +23,9 @@ python $BASE_DIR/manage.py loaddata 010_event 011_info
 python $BASE_DIR/manage.py loaddata 012_invitation
 
 ## gatheros_subscription
-python $BASE_DIR/manage.py loaddata 001_form 002_field 003_field_option
-python $BASE_DIR/manage.py loaddata 004_lot 005_subscription
+python $BASE_DIR/manage.py loaddata 001_default_field 002_form 003_field 004_field_option
+python $BASE_DIR/manage.py loaddata 005_lot 006_subscription
+#python $BASE_DIR/manage.py loaddata 007_answer
 
 # Atualizando a data dos eventos
 python $BASE_DIR/bin/update_data.py
