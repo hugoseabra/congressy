5e7ed22f3d0c40835d08568b99e236518c1831e5
Hugo Seabra <hugoseabra19@gmail.com>
2017-05-05 16:33:52 -0300

    gatheros_event: documentando ACL

---
+++ gatheros_event/acl/action.py
@@ -0,0 +1,37 @@
+"""
+ALGUMAS AÇÕES DENTRO DA PLATAFORMA SERÃO DE ACORDO COM A ORGANIZAÇÃO E GRUPO DO MEMBRO DENTRO DELA.
+
+- Grupos de memberos:
+    1. ADMIN - Administrador
+    2. HELPER - Auxiliar
+
+- Ações possíveis:
+    = Organização:
+        1. Retirar-se de organização - ADMIN | HELPER
+        2. Editar organização - ADMIN
+        3. Excluir organização - ADMIN
+        4. Transferir eventos - ADMIN
+    = Evento:
+        1. Criar evento - ADMIN
+        2. Editar evento - ADMIN | HELPER
+        3. Excluir evento - ADMIN
+        4. Publicar/Despublicar site evento  - ADMIN
+        5. Ativar/Desativar/Alterar tipo inscrições - ADMIN
+        6. Exportar dados - ADMIN
+    = Lote:
+        1. Criar lotes - ADMIN | HELPER
+        2. editar lotes - ADMIN  | HELPER
+        3. excluir lotes - ADMIN | HELPER
+    = Inscrição:
+        > Formulário:
+            1. Editar formulário - ADMIN | HELPER
+        > Pré-inscrição (antes da inicial do evento):
+            1. Cadastrar inscrição - ADMIN | HELPER
+            2. Editar inscrição - ADMIN | HELPER
+            3. Excluir inscrição - ADMIN | HELPER
+        > Credenciamento (depois da inicial do evento):
+            1. Cadastrar inscrição - ADMIN | HELPER
+            2. Editar inscrição - ADMIN | HELPER
+            3. Excluir inscrição - ADMIN | HELPER
+            4. Confirmar presença (check-in) - ADMIN | HELPER
+"""
