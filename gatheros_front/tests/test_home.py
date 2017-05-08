72fffa0e0eb6934a2a76c5df81e9a29c8c083c58
Hugo Seabra <hugoseabra19@gmail.com>
2017-05-08 14:16:12 -0300

    gatheros_front: corrigindo test_home.py

--- gatheros_front/tests/test_home.py
+++ gatheros_front/tests/test_home.py
@@ -4,7 +4,7 @@ from django.urls import  reverse
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.result = self.client.get(reverse('gatheros_front:home'))
+        self.result = self.client.get(reverse('gatheros_front:login'))
 
     def test_status_is_200_ok(self):
         self.assertEqual(self.result.status_code, 200)
