ccabe58d6156be3028e23593f4e49bcb37a564f5
Hugo Seabra <hugoseabra19@gmail.com>
2017-05-08 20:12:22 -0300

    - core: refatorando delete e user_context para view e form

--- gatheros_event/views/event/delete.py
+++ gatheros_event/views/event/delete.py
@@ -1,50 +1,28 @@
-# from django.http import Http404
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
 from core.view.delete import DeleteViewMixin
-from core.view.user_context import UserContextMixin
 from gatheros_event.models import Event, Member
 
 
-class EventDeleteView(LoginRequiredMixin, UserContextMixin, DeleteViewMixin):
+class EventDeleteView(DeleteViewMixin):
     model = Event
-    template_name = 'gatheros_event/event/event_confirm_delete.html'
+    template_name = 'gatheros_event/event/delete.html'
     success_url = reverse_lazy('gatheros_event:event-list')
+
     delete_message = "Não é possível excluir o evento '{name}' possivelmente" \
                      " porque há lotes com inscrições. Exporte, gerencie e" \
                      " exclua os lotes antes de excluir este evento."
 
-    def render_to_response( self, context, **response_kwargs ):
-        if not self.can_delete():
-            messages.success(
-                self.request,
-                "Você não tem permissão para excluir este evento"
-            )
-            return redirect(self.success_url.format(**self.object.__dict__))
+    not_allowed_message = "Você não tem permissão para excluir este evento"
 
-        return super(EventDeleteView, self).render_to_response(
-            context=context,
-            **response_kwargs
-        )
-
-    def post( self, request, *args, **kwargs ):
-        if not self.can_delete():
-            messages.success(
-                self.request,
-                "Você não tem permissão para excluir este evento"
-            )
-            return redirect(self.success_url.format(**self.object.__dict__))
-
-        return super(EventDeleteView, self).post(request, *args, **kwargs)
+    success_message = "Evento excluído com sucesso!"
 
     def can_delete( self ):
-        if self.super_user:
-            return True
+        event = self.get_object()
+        organization = event.organization
+        active_organization = self.user_context['active_organization']
+        active_member = self.user_context['active_member_group']
 
-        self.object = self.get_object()
-
-        return self.object.organization.pk == self.organization['pk'] \
-               or self.member_group['group'] == Member.ADMIN
+        return organization.pk == active_organization['pk'] and (
+            active_member and active_member['group'] == Member.ADMIN
+        )
--- gatheros_event/views/event/list.py
+++ gatheros_event/views/event/list.py
@@ -1,15 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
-from core.view.user_context import UserContextMixin
+from core.view.user_context import UserContextViewMixin
 from gatheros_event.models import Event, Member
 
 
-class EventListView(
-    LoginRequiredMixin,
-    UserContextMixin,
-    ListView
-):
+class EventListView(LoginRequiredMixin, UserContextViewMixin, ListView):
     model = Event
     template_name = 'gatheros_event/event/list.html'
     ordering = ['name']
@@ -18,12 +14,15 @@ class EventListView(
         qs = super(EventListView, self).get_queryset()
 
         # Super user vê tudo
-        if self.user_context['superuser']:
+        if self.user_context.get('superuser', False):
             return qs
 
-        return qs.filter(
-            organization__pk=self.user_context['active_organization']['pk']
-        )
+        try:
+            return qs.filter(
+                organization__pk=self.user_context['active_organization']['pk']
+            )
+        except TypeError:
+            pass
 
     def get_context_data( self, **kwargs ):
         context = super(EventListView, self).get_context_data(**kwargs)
@@ -33,21 +32,28 @@ class EventListView(
     def get_event_list_context( self ):
         member_group = self.user_context.get('active_member_group')
 
+        def can_add():
+            return self.user_context.get('superuser', False) or (
+                member_group and member_group['group'] in [
+                    Member.ADMIN,
+                ]
+            )
+
         def can_edit():
             return self.user_context.get('superuser', False) or (
                 member_group and member_group['group'] in [
                     Member.ADMIN,
-                    Member.HELPER
+                    Member.HELPER,
                 ]
             )
 
         def can_delete():
             return self.user_context.get('superuser', False) or (
-                member_group
-                and member_group.get('active_member_group') == Member.ADMIN
+                member_group and member_group['group'] == Member.ADMIN
             )
 
         return {
+            'can_add': can_add,
             'can_edit': can_edit,
             'can_delete': can_delete,
         }
