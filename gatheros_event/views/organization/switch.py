fc9cafb36eecfaffa7de2b029e31359bbd1ef38d
Diego Tolentino <diegotolentino@gmail.com>
2017-05-11 09:50:19 -0300

    Separando as responsabilidades e fazendo a sobreposição de cada metodo

--- gatheros_event/views/organization/switch.py
+++ gatheros_event/views/organization/switch.py
@@ -1,27 +1,24 @@
 from django.contrib import messages
 from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
-from django.shortcuts import get_object_or_404
 
-from gatheros_event.models import Organization
 from gatheros_event.acl.gatheros_user_context import update_user_context
+from gatheros_event.models import Organization
 
 
 class NotAllowedOrganization(IntegrityError):
-    def __init__( self, *args, **kwargs ):
+    def __init__(self, *args, **kwargs):
         super(NotAllowedOrganization, self).__init__(*args, **kwargs)
 
 
 # @TODO Refatorar para inserir dados em sessão e mudança de contexto
 
 class OrganizationSwitch(RedirectView):
-    def post( self, request, *args, **kwargs ):
+    def post(self, request, *args, **kwargs):
         pk = request.POST.get('organization-context-pk', None)
-        next_path = request.POST.get(
-            'next',
-            reverse_lazy('gatheros_front:home')
-        )
+
         if not pk:
             return
 
@@ -35,5 +32,11 @@ class OrganizationSwitch(RedirectView):
 
         messages.info(request, "Agora você {}.".format(context))
 
-        self.url = next_path
         return super(OrganizationSwitch, self).post(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return self.request.POST.get(
+            'next',
+            reverse_lazy('gatheros_front:home')
+        )
+
