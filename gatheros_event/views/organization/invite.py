613845154debba70aa2112af5f1246f0c274c8a8
Diego Tolentino <diegotolentino@gmail.com>
2017-05-18 10:14:08 -0300

    Form de convite

---
+++ gatheros_event/views/organization/invite.py
@@ -0,0 +1,38 @@
+from django.views.generic import TemplateView
+
+from gatheros_event.views.mixins import AccountMixin
+
+
+class OrganizationPanelView(AccountMixin, TemplateView):
+    template_name = 'gatheros_event/organization/invite.html'
+
+
+from gatheros_event.forms import InvitationForm
+from django.views.generic.edit import FormView
+
+
+class InviteView(FormView):
+    template_name = 'gatheros_event/organization/invite.html'
+
+    # form_class = InvitationForm
+    # success_url = '/thanks/'
+
+    def get_form(self, form_class=None):
+        """
+        Retorna uma instancia de form para ser usada na view
+        """
+        kwargs = self.get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return InvitationForm(**kwargs)
+
+    def form_valid(self, form):
+        """
+        No post executa este método se o form for válido
+
+        :param form:
+        :return:
+        """
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_invite()
+        return super(ContactView, self).form_valid(form)
