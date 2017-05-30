b710e11928e03072d0b31b64dce7d0b2d8427b1d
Diego Tolentino <diegotolentino@gmail.com>
2017-05-30 16:49:24 -0300

    Form de convite

--- gatheros_event/forms.py
+++ gatheros_event/forms.py
@@ -1,7 +1,12 @@
+from django.shortcuts import reverse
 from django import forms
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 from core.fields import MultiEmailField
-from .models import Organization
+from .models import Invitation, Organization
+from .models.rules import check_invite
 
 
 class InvitationForm(forms.Form):
@@ -10,15 +15,62 @@ class InvitationForm(forms.Form):
         empty_label='-----',
         label='Organização'
     )
-    emails = MultiEmailField(label='Emails')
+    to = MultiEmailField(label='Emails')
+    user = None
+    _invites = None
 
     def __init__(self, user, data=None, *args, **kwargs):
         super(InvitationForm, self).__init__(data=data, *args, **kwargs)
 
-        if not user.is_superuser:
-            self.fields['organization'].queryset = Organization.objects.filter(
-                members__person__user=user
+        organizations = Organization.objects.filter(
+            members__person__user=user
+        )
+
+        orgs_can_invite = organizations
+
+        for org in organizations:
+            if not user.has_perm('gatheros_event.can_invite', org):
+                orgs_can_invite = orgs_can_invite.exclude(pk=org.pk)
+
+        self.fields['organization'].queryset = orgs_can_invite
+        self.user = user
+        self._invites = []
+
+    def clean(self):
+        if self.errors:
+            return []
+
+        organization = self.cleaned_data['organization']
+
+        # Cria os convites
+        for email in self.cleaned_data['to']:
+            invited_user, _ = User.objects.get_or_create(
+                username=email,
+                defaults={'email': email}
+            )
+            invite = Invitation(
+                author=organization.get_members(person=self.user)[0],
+                to=invited_user,
+                type=Invitation.INVITATION_TYPE_HELPER
             )
 
-    def save(self):
-        pass
+            # Submete os convites as regras
+            check_invite(invite)
+
+            # Adiciona na lista dos convites que estão ok
+            self._invites.append(invite)
+
+    def send_invite(self):
+        for invite in self._invites:
+            url = reverse(
+                'gatheros_event:organization-invite-accept',
+                kwargs={'pk': str(invite.pk)}
+            )
+            invite.to.save()
+            invite.save()
+            send_mail(
+                'Novo convite para organização',
+                'Para aceitar clique no link: {0}'.format(url),
+                settings.DEFAULT_FROM_EMAIL,
+                [invite.to.email]
+            )
