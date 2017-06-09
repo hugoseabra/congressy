2ce7c77cd2fd9f59f78c543172785892c3342a12
Hugo Seabra <hugoseabra19@gmail.com>
2017-06-09 14:41:54 -0300

    Permissão de visualização em views de Organization, Inviate e Place

--- gatheros_event/views/organization/panel.py
+++ gatheros_event/views/organization/panel.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 from gatheros_event.views.mixins import AccountMixin
@@ -7,3 +10,18 @@ from gatheros_event.views.mixins import AccountMixin
 
 class OrganizationPanelView(AccountMixin, TemplateView):
     template_name = 'gatheros_event/organization/panel.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        dispatch = super(OrganizationPanelView, self).dispatch(
+            request,
+            *args,
+            **kwargs
+        )
+        if not self._can_view():
+            messages.warning(request, 'Você não está em uma organização.')
+            return redirect(reverse_lazy('gatheros_front:start'))
+
+        return dispatch
+
+    def _can_view(self):
+        return self.organization.internal is False
