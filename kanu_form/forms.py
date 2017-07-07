994e617a86c62a1c9d0e432078c7d8bc303595dd
Hugo Seabra <hugoseabra19@gmail.com>
2017-07-07 19:27:21 -0300

    Melhoria em kanu_form/forms.py

--- kanu_form/forms.py
+++ kanu_form/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import model_to_dict
 from django.utils import six
 
 
@@ -17,3 +18,34 @@ class KanuForm(forms.Form):
         for name, django_field in six.iteritems(self.field_manager.fields):
             self.fields[name] = django_field
             self.fields.keyOrder.append(name)
+
+    @staticmethod
+    def get_field_dict(gatheros_field):
+        """
+        Resgata dict de `Field` com a estrutura correta.
+
+        :param gatheros_field: `gatheros_subscription.models.Field`
+        :return: Dict
+        """
+        field_dict = model_to_dict(gatheros_field, exclude=[
+            'active',
+            'default_value',
+            'form_default_field',
+            'form',
+            'id',
+            'instruction',
+            'order',
+            'with_options',
+        ])
+
+        field_dict['help_text'] = gatheros_field.instruction
+
+        if gatheros_field.default_value:
+            field_dict['initial'] = gatheros_field.default_value
+
+        if gatheros_field.with_options:
+            field_dict['options'] = [
+                (opt.value, opt.name) for opt in gatheros_field.options.all()
+            ]
+
+        return field_dict
