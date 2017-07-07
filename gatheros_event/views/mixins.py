b01af2704455daf66d7b61ddb7a8a39aab0ec821
Diego Tolentino <diegotolentino@gmail.com>
2017-07-07 15:26:48 -0300

    Adicionado FormListViewMixin que é um mixin com form e que ajuda a filtrar a lista

--- gatheros_event/views/mixins.py
+++ gatheros_event/views/mixins.py
@@ -2,10 +2,14 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 from django.forms.models import model_to_dict
+from django.http import Http404
 from django.shortcuts import redirect
 from django.utils.functional import SimpleLazyObject
+from django.utils.translation import ugettext as _
 from django.views.generic import DeleteView
 from django.views.generic.base import View
+from django.views.generic.edit import FormMixin
+from django.views.generic.list import ListView
 
 from core.model.deletable import DeletableModelMixin
 from gatheros_event.helpers.account import (
@@ -190,3 +194,29 @@ class DeleteViewMixin(AccountMixin, DeleteView):
     def post_delete(self):
         """ Processo a ser executado depois de deletar. """
         pass
+
+
+class FormListViewMixin(FormMixin, ListView):
+    """
+    Lista com formulário
+    """
+
+    def get(self, request, *args, **kwargs):
+        # From ProcessFormMixin
+        form_class = self.get_form_class()
+        self.form = self.get_form(form_class)
+
+        # From BaseListView
+        self.object_list = self.get_queryset()
+        allow_empty = self.get_allow_empty()
+        if not allow_empty and len(self.object_list) == 0:
+            raise Http404(
+                _(u"Empty list and '%(class_name)s.allow_empty' is False.")
+                % {'class_name': self.__class__.__name__})
+
+        context = self.get_context_data(object_list=self.object_list,
+                                        form=self.form)
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
