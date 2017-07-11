cc3df2c2c03b6b1933a47f0047cb80e8cde940fd
Hugo Seabra <hugoseabra19@gmail.com>
2017-07-11 18:48:24 -0300

    Melhoria e kanu_form/forms

--- kanu_form/forms.py
+++ kanu_form/forms.py
@@ -33,3 +33,37 @@ class KanuForm(forms.Form):
         self.kanu_fields.update({name: field})
 
         return field.get_django_field()
+
+    def set_attr(self, field_name, name, value):
+        """
+        Seta atributo em um campo do formulário.
+        :param field_name: nome do campo
+        :param name: nome do atributo
+        :param value: valor do atributo
+        """
+        field = self.get_field_by_name(field_name)
+        field.widget.attrs.update({name: value})
+
+    def unset_attr(self, field_name, name):
+        """
+        Remove atributo em um campo do formulário.
+        :param field_name: nome do campo
+        :param name: nome do atributo
+        """
+        field = self.get_field_by_name(field_name)
+        attrs = field.widget.attrs
+        attr = attrs.get(name)
+        if attr:
+            del attrs[name]
+
+        field.widget.attrs = attrs
+
+    def get_field_by_name(self, name):
+        field = self.fields.get(name)
+        if not field:
+            raise Exception(
+                'Não foi possível encontrar um campo com o nome'
+                ' `{}`.'.format(name)
+            )
+
+        return field
