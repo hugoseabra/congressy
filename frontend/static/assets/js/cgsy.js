df8bb70570a94339a01ccb7e5919de8f9fe1127b
Hugo Seabra <hugoseabra19@gmail.com>
2017-12-26 17:41:02 -0200

    - Ajudando uso do bloco 'head' que não mais existe; - Voltando datatbles para a versão do template; - Refactoring em datatables de organização; - Padrão de filtro de lista;

--- frontend/static/assets/js/cgsy.js
+++ frontend/static/assets/js/cgsy.js
@@ -1,4 +1,4 @@
-// Mobile top Menu
+//========================= MOBILE TOP MENU =================================//
 $(document).ready(function() {
     $('#mobile-top-menu-button').click(function() {
         var block = $("#mobile-top-menu");
@@ -15,3 +15,59 @@ $(document).ready(function() {
         block.animate({"right": right}, 300);
     });
 });
+
+//========================= CAIXA DE FILTROS ================================//
+window.cgsy = window.cgsy || {};
+(function(window, $) {
+    window.cgsy.Filter = {
+        init: function(button_el, box_el, active) {
+            this.button_el = $(button_el);
+            this.box_el = $(box_el);
+            this.active = active === true;
+
+            if (active) {
+                this.show(false);
+            } else {
+                this.hide(false);
+            }
+
+            return this;
+        },
+        show: function(fade) {
+            fade = fade === true;
+            if (!fade) {
+                this.box_el.show();
+            } else {
+                this.box_el.fadeIn();
+            }
+
+            this.button_el.addClass('active');
+            this.button_el.removeClass('btn-default');
+            this.button_el.addClass('btn-primary');
+
+            var that = this;
+            this.button_el.unbind('click');
+            this.button_el.on('click', function() {
+                that.hide(true);
+            });
+        },
+        hide: function(fade) {
+            fade = fade === true;
+            if (!fade) {
+                this.box_el.hide();
+            } else {
+                this.box_el.fadeOut();
+            }
+
+            this.button_el.removeClass('active');
+            this.button_el.addClass('btn-default');
+            this.button_el.removeClass('btn-primary');
+
+            var that = this;
+            this.button_el.unbind('click');
+            this.button_el.on('click', function() {
+                that.show(true);
+            });
+        }
+    };
+})(window, jQuery);
