702bba911a2a8fcb44347c98c174fc7be4b5fd3c
Nathan Martins <nathan.eua@gmail.com>
2017-12-28 17:45:35 -0200

    - Lote Forms   - Se primeiro lote, permitir editar campo de data inicial . A data final (não editável) será por padrão, 1 minuto antes do início do evento.
     - Se houver lote anterior, exibir campo de data inicial (editável). A data final (não editável) será por padrão, 1 minuto antes do início do evento.
    
     - Caso exista lote anterior, a data inicial a inserir será sempre 1 dia após a data inicial do lote anterior.

--- gatheros_subscription/forms/lot.py
+++ gatheros_subscription/forms/lot.py
@@ -1,8 +1,10 @@
 """ Formulários de `Lot` """
 import uuid
+from datetime import timedelta, datetime
 from django import forms
 
 from gatheros_subscription.models import Lot
+from datetimewidget.widgets import DateTimeWidget
 
 
 class LotForm(forms.ModelForm):
@@ -28,7 +30,7 @@ class LotForm(forms.ModelForm):
         ]
         widgets = {'event': forms.HiddenInput(),
                    'price': forms.TextInput(),
-
+                   'date_start': DateTimeWidget(),
                    }
 
     def __init__(self, **kwargs):
@@ -36,11 +38,17 @@ class LotForm(forms.ModelForm):
         kwargs['initial']['exhibition_code'] = str(uuid.uuid4()).split(
             '-')[0].upper()
 
+        event = kwargs.get('initial').get('event')
+
+        kwargs['initial']['date_end'] = event.date_start - timedelta(minutes=1)
+
         super(LotForm, self).__init__(**kwargs)
 
         self.event = kwargs.get('initial').get('event')
 
         self._set_dates_help_texts()
+
+        self._set_widget_start_date()
 
     def _set_dates_help_texts(self):
         last_lot = self.event.lots.last()
@@ -59,6 +67,29 @@ class LotForm(forms.ModelForm):
             self.fields['date_start'].help_text = date_start_help
 
         event_date_start = self.event.date_start.strftime('%d/%m/%Y %Hh%M')
+        lot_last_call = self.event.date_start - timedelta(minutes=1)
         self.fields['date_end'].help_text = \
             'O evento inicia-se em {}. O final do lote deve ser anterior a' \
             ' esta data.'.format(event_date_start)
+
+    def _set_widget_start_date(self):
+
+        previous_lot_exist = self.event.lots.last()
+        last_call = self.event.date_start - timedelta(minutes=1)
+        if previous_lot_exist:
+            if previous_lot_exist.date_start < datetime.now():
+                str_date = '{0:%d-%m-%Y %H:%M}'.format(datetime.now())
+            else:
+                str_date = '{0:%d-%m-%Y %H:%M}'.format(
+                    previous_lot_exist.date_start + timedelta(days=1))
+            last_date_str = '{0:%d-%m-%Y %H:%M}'.format(last_call)
+            self.fields['date_start'].widget = DateTimeWidget(
+                options={'startDate': str_date, 'endDate': last_date_str})
+
+    def clean_date_start(self):
+        last_lot = self.event.lots.last()
+        date_start = self.cleaned_data['date_start']
+        last_lot.date_end = date_start - timedelta(minutes=1)
+        last_lot.save()
+        return date_start
+
