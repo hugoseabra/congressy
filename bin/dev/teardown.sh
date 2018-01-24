062841d3edae745a2a826ad99299d8d872720ea9
Nathan Martins <nathan.eua@gmail.com>
2018-01-24 16:29:12 -0200

    Updating teardown script

--- bin/dev/teardown.sh
+++ bin/dev/teardown.sh
@@ -3,5 +3,14 @@
 
 set -ex
 
+export DJANGO_SETTINGS_MODULE=project.settings.dev
+
+export BASE_DIR=`python -c "
+import $DJANGO_SETTINGS_MODULE as settings
+print(settings.BASE_DIR)"`
+
+export PYTHONPATH=$BASE_DIR
+
+
 docker-compose -f $BASE_DIR/bin/dev/docker-compose.yml down
 docker volume prune -f
