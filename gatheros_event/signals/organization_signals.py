f5767a2d9ed2a7f946628cf7e67570e9b1dcc420
Nathan Martins <nathan.eua@gmail.com>
2018-01-24 16:32:48 -0200

    Cleaning up signals file and moving the bank account and recipiente creation to the form

--- gatheros_event/signals/organization_signals.py
+++ gatheros_event/signals/organization_signals.py
@@ -1,11 +1,10 @@
 """ Signals do model `Organization`. """
-import pagarme
-from django.conf import settings
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import six
 
-from gatheros_event.models import Person, Organization
+from gatheros_event.models import Person
 
 
 @receiver(post_save, sender=Person)
@@ -28,53 +27,3 @@ def update_person_related_organization(instance, raw, **_):
 
         organization.save()
 
-
-@receiver(post_save, sender=Organization)
-def create_financial_org(instance, raw, **_):
-    """
-    Atualiza nomes de Organização e Usuário assim que o nome da
-    pessoa é atualizado.
-    """
-
-    # Disable when loaded by fixtures
-    if raw is True:
-        return
-
-    required_data = ['bank_code',
-                     'agency',
-                     'account',
-                     'legal_name',
-                     'account_type',
-                     'cnpj_ou_cpf', ]
-
-    try:
-
-        for item in required_data:
-            getattr(instance, item)
-
-
-        params = {
-            'agencia': instance.agency,
-            'bank_code': instance.bank_code,
-            'conta': instance.account,
-            'document_number': instance.cnpj_ou_cpf,
-            'legal_name': instance.legal_name
-        }
-
-        if hasattr(instance, 'agencia_dv'):
-            params['agencia_dv'] = instance.agencia_dv
-
-        if hasattr(instance, 'conta_dv'):
-            params['conta_dv'] = instance.conta_dv
-
-        pagarme.authentication_key(settings.PAGARME_API_KEY)
-
-        bank_account = pagarme.bank_account.create(params)
-
-        if bank_account:
-            instance.active_bank_account = True
-
-        instance.save()
-
-    except AttributeError:
-        pass
