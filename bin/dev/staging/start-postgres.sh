d25d8cf802d2c5e2f3ac43715ffdb7e160142ac2
Hugo Seabra <hugoseabra19@gmail.com>
2018-01-30 19:15:02 -0200

    Organizando backup e restauração de banco.

---
+++ bin/dev/staging/start-postgres.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+##############################################################################
+# CUIDADO!
+# Esta script será executado dentro do container do postgres.
+##############################################################################
+
+docker-compose -f /tmp/bkp/docker-compose.yml down
+docker-compose -f ./bin/dev/docker-compose.yml up -d
