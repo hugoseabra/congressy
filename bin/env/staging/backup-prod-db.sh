c7cac5974d60cfa3a34d00cef87c9c4b086a57bd
Hugo Seabra <hugoseabra19@gmail.com>
2018-02-01 18:24:29 -0200

    CI: Backup de banco de dados agora é feito apenas uma vez por dia.

---
+++ bin/env/staging/backup-prod-db.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+set -e
+
+##############################################################################
+# CUIDADO!
+# Esta script será executado dentro do container do postgres.
+##############################################################################
+
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+BKP_PATH="/tmp/bkp/backup"
+
+# Cria diretório onde estará o backup
+mkdir -p ${BKP_PATH}
+
+BKP_FILE_NAME="backup-`date +'%Y-%m-%d'`.sql"
+BKP_FILE_PATH="$BKP_PATH/$BKP_FILE_NAME"
+
+if [ ! -f "$BKP_FILE_PATH" ]; then
+    # Dump do DB
+    PGPASSWORD=4UnADjyMjeeB7GSc pg_dump \
+          --host congressy.cy6gssymlczu.us-east-1.rds.amazonaws.com \
+          --port 5499 \
+          --username congressy \
+          --format=c \
+          --verbose \
+          cgsyplatform > ${BKP_FILE_PATH}
+fi
+
+# Tamanho do arquivo
+echo "Backup size: `du -h ${BKP_FILE_PATH}`"
+
+# Assegura que o postgres irá processar o arquivo colocando o bash em um
+# local onde onde será contruído o volume do container do cgsy-postgres
+cp "${BASE}/bin/env/staging/db-restore.sh" /tmp/bkp/.
