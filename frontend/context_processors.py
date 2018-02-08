06e043d236c92fbf9a347cef4ba8b286720bd483
Hugo Seabra <hugoseabra19@gmail.com>
2018-02-08 18:57:14 -0200

    - hotjar; - tracker para mapear erros de javascript; - context_processor.sentry_public_dsn para produção

---
+++ frontend/context_processors.py
@@ -0,0 +1,14 @@
+"""
+Sentry public DSN para suporte a erros do javascript
+"""
+import os
+
+
+def sentry_public_dsn(request):
+    """
+    Adicionar SENTRY_PUBLIC_DSN ao contexto de templates.
+    """
+
+    return {
+        'SENTRY_PUBLIC_DSN': os.getenv('SENTRY_PUBLIC_DSN'),
+    }
