89caa65a760f56f05325f4f087d72fa4e0f2c50e
Hugo Seabra <hugoseabra19@gmail.com>
2018-02-09 15:32:54 -0200

    Deploy staging e produção atualizando Site ID.

--- conf/staging/container-entry.sh
+++ conf/staging/container-entry.sh
@@ -70,6 +70,7 @@ run_python_script "Configurando SETTINGS" /configure-settings.py
 run_python_script "Configurando VERSÃO" /configure-version.py
 run_python_script "Coletando arquivos estáticos" "manage.py collectstatic --noinput --verbosity 0"
 run_python_script_with_output "Executando migrate" "manage.py migrate"
+run_python_script_with_output "Atualizando Site ID" "manage.py loaddata 000_site_staging"
 run_python_script_with_output "Populando banco de dados" "manage.py loaddata logtailer_filter logtailer_logfile"
 
 run_bash_script_with_output "Baixando arquivos do S3" /in-sync.sh
