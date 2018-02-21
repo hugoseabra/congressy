ae2e2f9c5591bd8e367698275718db3bb10f5671
Hugo Seabra <hugoseabra19@gmail.com>
2018-02-21 14:31:45 -0300

    - Organizando bibliotecas auxiliares; - Organizando context_processors; - Inserindo parâmetros no Tawk.to

--- frontend/context_processors.py
+++ frontend/context_processors.py
@@ -2,26 +2,13 @@
 Sentry public DSN para suporte a erros do javascript
 """
 import os
-import hmac
 
 
-def sentry_public_dsn(request):
+def sentry_public_dsn():
     """
     Adicionar SENTRY_PUBLIC_DSN ao contexto de templates.
     """
+
     return {
         'SENTRY_PUBLIC_DSN': os.getenv('SENTRY_PUBLIC_DSN'),
-    }
-
-
-def tawkto_secure_hash(request):
-    """
-    Adicionar hash de segurança do Tawkto utilizando a chave de api
-    em TAWTO_API_KEY.
-    """
-
-    hmac.new("sharedpassword", "", hashlib.sha256).hexdigest()
-
-    return {
-        'TAWTO_SECURE_HASH': os.getenv('SENTRY_PUBLIC_DSN'),
     }
