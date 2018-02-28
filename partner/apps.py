b40994867f6ac41851373cd60cb12d747f27248d
Nathan Martins <nathan.eua@gmail.com>
2018-02-28 15:36:41 -0300

    Partner: Adding Signals

--- partner/apps.py
+++ partner/apps.py
@@ -5,4 +5,8 @@ class PartnerConfig(AppConfig):
     name = 'partner'
     verbose_name = 'Parceiros'
 
+    # noinspection PyUnresolvedReferences
+    def ready(self):
+        import partner.signals
+
 
