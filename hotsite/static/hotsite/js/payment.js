979cee598554d46ce25dfbf0ec269afa23a48979
Hugo Seabra <hugoseabra19@gmail.com>
2018-02-28 20:35:16 -0300

    Melhoria: suporte a parcelamento:  - template pronto;  - domínio pronto;  - formulário de lote pronto;

--- hotsite/static/hotsite/js/payment.js
+++ hotsite/static/hotsite/js/payment.js
@@ -6,14 +6,32 @@ function hide_payment_elements() {
     $('#id_button_pay').show();
 }
 
-function process_payment(encryption_key, event_name, lots, transactions) {
-    var amount_id = $('#id_lot').val().toString();
-    var amount = lots[amount_id] * 100;
+function process_payment(
+    encryption_key,
+    interest_rate,
+    event_name,
+    lots,
+    transactions
+) {
+    if (!interest_rate) {
+        alert('Erro: fale com o suporte.');
+        Raven.captureException(e);
+        return;
+    }
+
     var billing_title = 'Inscrição do evento: ' + event_name;
+    var amount_id = $('#id_lot').val().toString();
+    var lot = lots[amount_id];
+    var amount = lot.amount * 100;
+    var allow_installment = lot.allow_installment === true;
+    var installment_limit = lot.installment_limit;
+    var free_installment = parseInt(lot.free_installment);
 
     transactions = transactions || 'credit_card,boleto';
+    allow_installment = allow_installment === true;
 
     function handleSuccess(data) {
+        console.log(data);
         hide_payment_elements();
 
         switch (data.payment_method) {
@@ -24,7 +42,13 @@ function process_payment(encryption_key, event_name, lots, transactions) {
                 $('#id_credit_card').show();
                 break;
             default:
-                console.error('Unsupported payment type: ' + data.payment_method);
+                var msg = 'Unsupported payment type: ' + data.payment_method;
+                Raven.captureException(msg);
+                alert(
+                    "Ocorreu um erro durante o processamento," +
+                    " tente novamente depois."
+                );
+                Raven.showReportDialog();
         }
 
         $('#payment_buttons').show();
@@ -33,12 +57,13 @@ function process_payment(encryption_key, event_name, lots, transactions) {
         $('#id_transaction_type').val(data.payment_method);
         $('#id_amount').val(data.amount);
         $('#id_card_hash').val(data.card_hash);
+        $('#id_installments').val(data.installments);
     }
 
     function handleError(data) {
         if(TRACKER_CAPTURE && Raven){
-            Raven.showReportDialog();
             Raven.captureException(JSON.stringify(data));
+            Raven.showReportDialog();
         } else {
             alert(
                 "Ocorreu um erro durante o processamento," +
@@ -53,11 +78,12 @@ function process_payment(encryption_key, event_name, lots, transactions) {
         error: handleError
     });
 
-    checkout.open({
+    var params = {
         amount: amount,
         createToken: 'false',
         paymentMethods: transactions,
         customerData: false,
+        interestRate: interest_rate,
         items: [
             {
                 id: '1',
@@ -67,7 +93,20 @@ function process_payment(encryption_key, event_name, lots, transactions) {
                 tangible: false
             }
         ]
-    });
+    };
+
+    if (allow_installment) {
+        if (parseInt(installment_limit) > 10) {
+            installment_limit = 10;
+        }
+        params['maxInstallments'] = parseInt(installment_limit) || 10;
+
+        if (parseInt(free_installment) > 0) {
+            params['freeInstallments'] = parseInt(free_installment);
+        }
+    }
+
+    checkout.open(params);
 }
 
 $('#id_remove').on('click', function () { hide_payment_elements(); });
