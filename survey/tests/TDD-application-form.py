d638b2bf0fc801cac0184742cddbd32acf421d5b
Hugo Seabra <hugoseabra19@gmail.com>
2018-03-09 13:59:59 -0300

    Documentação de TDD pronta.

---
+++ survey/tests/TDD-application-form.py
@@ -0,0 +1,17 @@
+"""
+Testes de aplicação do módulo Survey conforme documentação.
+"""
+
+from django.test import TestCase
+
+
+class SurveyOptionTest(TestCase):
+    """
+    Testes de ModelForm de Opção de perguntas
+    """
+    def first_blank_option(self):
+        """
+        Testa opçaõ do usuário quando ele decide ter o primeiro a primeira
+        opção em branco.
+        """
+        self.fail('not implmented')
