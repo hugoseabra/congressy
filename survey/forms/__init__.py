05ccb6fab01c2ddfe1e48f60681b8b67e71bf5e1
Nathan Martins <nathan.eua@gmail.com>
2018-03-13 14:08:00 -0300

    Survey: Retirando antigos forms.

--- survey/forms/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-    Survey Module forms
-"""
-
-from .question import QuestionModelForm, NewSimpleQuestionForm, \
-    NewComplexQuestionForm
-from .option import OptionModelForm
-from .answer import AnswerModelForm
-from .survey import SurveyModelForm
--- survey/tests/domain/forms/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-    Survey domain form tests
-"""
