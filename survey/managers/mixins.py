a5577976abac5810213982f2bf90c7fc62a95956
Hugo Seabra <hugoseabra19@gmail.com>
2018-03-15 18:00:48 -0300

    Melhoria: init do manager.

--- survey/managers/mixins.py
+++ survey/managers/mixins.py
@@ -7,10 +7,28 @@ por:
 """
 
 from django import forms
+from django.forms.utils import ErrorList
 
 
 class Manager(forms.ModelForm):
     """
     Manager
     """
-    pass
+
+    def __init__(self, **kwargs):
+
+        data = kwargs.get('data')
+        files = kwargs.get('files')
+        auto_id = kwargs.get('auto_id', 'id_%s')
+        prefix = kwargs.get('prefix')
+        initial = kwargs.get('initial')
+        error_class = kwargs.get('error_class', ErrorList)
+        label_suffix = kwargs.get('label_suffix')
+        empty_permitted = kwargs.get('empty_permitted', False)
+        use_required_attribute = kwargs.get('use_required_attribute')
+
+        super().__init__(data=data, files=files, auto_id=auto_id,
+                         prefix=prefix, initial=initial,
+                         error_class=error_class, label_suffix=label_suffix,
+                         empty_permitted=empty_permitted,
+                         use_required_attribute=use_required_attribute)
