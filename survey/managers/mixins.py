474344a4b0150f5738e9359da1143dadb8f7fb8e
Nathan Martins <nathan.eua@gmail.com>
2018-03-15 10:28:39 -0300

    Survey: Implementando a checagem dos kwargs no Manager Mixin, para que o         mesmo aceite apenas kwargs com instancias e não         referencias('pk') ou outros atributos identificadores. Além de         colocar tests de coverage que cubra  100% as linhas.

--- survey/managers/mixins.py
+++ survey/managers/mixins.py
@@ -13,4 +13,97 @@ class Manager(forms.ModelForm):
     """
     Manager
     """
-    pass
+
+    def __init__(self, **kwargs):
+        """
+            TODO: Melhorar o comportamento deste:
+                Caveat deste comportamento, quando alguém implementa este
+                manager e criar uma assinatura diferente desta padrão como:
+                '__init__(self, extra_field, **kwargs)'
+
+                È então necessario dentro deste init dar um update no kwargs
+                que será repassado a implementação base do manager(essa
+                classe em si), da seguinte forma:
+
+                'kwargs.update({'extra_required_field': extra_required_field})'
+
+                Ao passar pelo construtor do manager, este kwarg ficará
+                perdido, sendo necessario setar ele na mão como atributo do
+                manager para poder acessa-lo depois, da seguinte forma:
+
+                'self.extra_required_field = extra_required_field'
+
+                Esse comportamento se torna necessario pois, esse atributo
+                será usado para no metodo 'save' visto que esse atributo não
+                está vinculado ao atributo 'self.instance'
+
+        """
+
+        self._check_argument_instances(kwargs)
+        kwargs = self._clear_arguments(kwargs)
+        super().__init__(**kwargs)
+
+    def _check_argument_instances(self, arguments):
+        """
+        Verifica se os argumentos passados são relativos a algum campo
+        relacional do model do Manager, verifica se o mesmo é um objeto
+        da instância relacionada.
+        """
+        model_class = self.Meta.model
+        ignored_keys = ('data',
+                        'files',
+                        'auto_id',
+                        'prefix',
+                        'initial',
+                        'instance',
+                        'error_class',
+                        'label_suffix',
+                        'empty_permitted',
+                        'field_order',
+                        'use_required_attribute',)
+
+        related_classes = {}
+        errors = {}
+
+        # Varre todos os campos do model definidos em Meta procurando por
+        # instancias relacionais que podem estar na lista de argumentos
+        # 'arguments'
+        for f in model_class._meta.get_fields():
+            if f.is_relation and f.many_to_one:
+                related_classes[f.name] = f.related_model
+
+        # Percorre todos os argumentos contidos em kwargs que são enviados
+        # via serviços a procura por algum campo relacional.
+        for key, value in arguments.items():
+
+            # Ignorando  as kwargs padrões.
+            if key in ignored_keys:
+                continue
+
+            # Procurando por algum classe que seja uma das relacionais.
+            if key in related_classes:
+                related = related_classes.get(key)
+                if not isinstance(value, related):
+                    errors[f.name] = f.related_model
+
+        if errors:
+            msg = []
+            for key, related_class in errors.items():
+                msg.append('"{}" expects, "{}" instance'.format(key,
+                                                            related_class))
+
+            raise TypeError(
+                'Argumentos não são instâncias de modelos relacionados:'
+                ' {}'.format(', '.join(msg))
+            )
+
+    def _clear_arguments(self, kwargs):
+        """ Limpa argumentos de campos relacionados a model do Manager. """
+        model_class = self.Meta.model
+
+        for f in model_class._meta.get_fields():
+            if f.name in kwargs and f.is_relation and f.many_to_one:
+                del kwargs[f.name]
+
+        return kwargs
+
--- survey/tests/domain/managers/test_manager_mixin.py
+++ survey/tests/domain/managers/test_manager_mixin.py
@@ -7,83 +7,205 @@ from django.db import models
 from test_plus.test import TestCase
 
 from survey.managers import Manager
+from faker import Faker
 from django import forms
 
 
-class ApplicationServiceFakeModel(f.FakeModel):
+class ApplicationServiceRelatedModel(f.FakeModel):
     name = models.CharField(max_length=100)
 
 
-@ApplicationServiceFakeModel.fake_me
+class ApplicationServiceRelatedRequiredModel(f.FakeModel):
+    name = models.CharField(max_length=100)
+
+
+class ApplicationServiceModel(f.FakeModel):
+    name = models.CharField(max_length=100)
+
+    extra_field = models.ForeignKey(
+        ApplicationServiceRelatedModel,
+        null=True,
+    )
+
+    extra_required_field = models.ForeignKey(
+        ApplicationServiceRelatedRequiredModel
+    )
+
+
+@ApplicationServiceModel.fake_me
+@ApplicationServiceRelatedModel.fake_me
+@ApplicationServiceRelatedRequiredModel.fake_me
 class ApplicationServiceMixinTest(TestCase):
     """ Main test implementation """
 
     class CorrectModelManager(Manager):
         class Meta:
-            model = ApplicationServiceFakeModel
+            model = ApplicationServiceModel
             fields = '__all__'
 
-    class IncorrectModelManager(forms.ModelForm):
+    class IncorrectModelManager(Manager):
         class Meta:
-            model = ApplicationServiceFakeModel
+            model = ApplicationServiceModel
             fields = '__all__'
 
-        def is_valid(self):
-            return False
+    class ExcludedModelManager(Manager):
+        class Meta:
+            model = ApplicationServiceModel
+            exclude = (
+                'extra_required_field',
+            )
+
+        def __init__(self, extra_required_field, **kwargs):
+            self.extra_required_field = extra_required_field
+            kwargs.update({'extra_required_field': extra_required_field})
+            super().__init__(**kwargs)
+
+        def save(self, commit=True):
+            self.instance.extra_required_field = self.extra_required_field
+            return super().save(commit)
 
     def setUp(self):
-        # Create a dummy model which extends the mixin
+        self.faker = Faker('pt_BR')
         self.correct_manager = self.CorrectModelManager
         self.incorrect_manager = self.IncorrectModelManager
+        self.excluded_manager = self.ExcludedModelManager
 
-    def test_manager_is_valid(self):
+        self.related_model = ApplicationServiceRelatedModel.objects.create(
+            name=self.faker.name()
+        )
+        self.required_model = \
+            ApplicationServiceRelatedRequiredModel.objects.create(
+                name=self.faker.name()
+            )
+
+    def test_manager_validation(self):
+        """
+            Valida se o manager respeita as regras do model ao qual ele foi
+            criado, além de respeitar as regras de definição do manager.
+        """
         manager = self.correct_manager
+        excluded_manager = self.excluded_manager
 
         instance_with_data = manager(
             data={
-                'name': 'Jon Snow'
-            }
+                'name': 'Jon Snow',
+                'extra_field': self.related_model.pk,
+                'extra_required_field': self.required_model.pk,
+            },
         )
 
         instance_with_no_data = manager(
             data={
-                'name': None
+                'name': None,
+                'extra_field': None,
+                'extra_required_field': None,
             }
         )
 
-        self.assertTrue(instance_with_data.is_valid())
+        instance_with_excluded_data = excluded_manager(
+            data={
+                'name': 'Jon Stark',
+                'extra_field': self.related_model.pk,
 
-        # Verifica se o que está faltando é de fato o nome.
+            },
+            extra_required_field=self.required_model,
+        )
+
+        # Verificando que caso seja passado uma referencia('pk') via kwargs,
+        # o manager não irá se construir corretamente.
+        # Managers trabalham apenas com instancias e não com referencias('pk')
+        with self.assertRaises(TypeError):
+            excluded_manager(
+                data={
+                    'name': 'Jon Snow',
+                    'extra_field': self.related_model.pk,
+
+                },
+                extra_required_field=self.required_model.pk,
+            )
+
+        # Validação das instancias corretas.
+        self.assertTrue(instance_with_data.is_valid())
+        self.assertTrue(instance_with_excluded_data.is_valid())
+
+        # Verifica os erros
         self.assertFalse(instance_with_no_data.is_valid())
         error_dict = instance_with_no_data.errors.as_data()
         error_list = list(error_dict)
 
-        self.assertEqual(error_list[0], 'name')
-        self.assertIsInstance(error_dict['name'][0], forms.ValidationError)
+        # Verificando que temos três itens faltando
+        self.assertIs(3, len(error_list))
 
-    def test_manager_editing(self):
+        # Verificando o teor dos erros.
+        for _, value in error_dict.items():
+            self.assertIsInstance(value[0], forms.ValidationError)
+            self.assertEqual(value[0].message, 'This field is required.')
 
-        manager = self.correct_manager
+        # Verificando a capacidade de persistencia de managers com exclusões
+        instance_with_excluded_data.save()
+        ApplicationServiceModel.objects.get(name='Jon Stark')
 
-        name = 'Jon Snow'
-        new_name = 'Jon Targaryn'
-
-        inital_instance = manager(
-            data={
-                'name': name
-            }
-        )
-
-        self.assertTrue(inital_instance.is_valid())
-        inital_instance.save()
-
-        persisted_instance = ApplicationServiceFakeModel.objects.get(name=name)
-
-        editing_instance = manager(
-            data={
-                'name': new_name
-            },
-            instance=persisted_instance,
-        )
-        self.assertTrue(editing_instance.is_valid())
-        editing_instance.save()
+    # def test_manager_editing(self):
+    #     manager = self.correct_manager
+    #
+    #     name = 'Jon Snow'
+    #     new_name = 'Jon Targaryn'
+    #
+    #     initial_instance = manager(
+    #         data={
+    #             'name': name,
+    #             'extra_field': self.related_model.pk,
+    #             'extra_required_field': self.required_model.pk,
+    #         }
+    #     )
+    #
+    #     self.assertTrue(initial_instance.is_valid())
+    #
+    #     initial_instance.save()
+    #
+    #     persisted_instance = ApplicationServiceModel.objects.get(name=name)
+    #
+    #     editing_instance = manager(
+    #         data={
+    #             'name': new_name,
+    #             'extra_field': self.related_model.pk,
+    #             'extra_required_field': self.required_model.pk,
+    #         },
+    #         instance=persisted_instance,
+    #     )
+    #     self.assertTrue(editing_instance.is_valid())
+    #     editing_instance.save()
+    #
+    #     # Validando se for persistido.
+    #     checking_persisted_instance = ApplicationServiceModel.objects.get(
+    #         pk=persisted_instance.pk)
+    #
+    #     self.assertEqual(new_name, checking_persisted_instance.name)
+    #
+    # def test_passing_reference_kwarg(self):
+    #     regular_manager = self.correct_manager
+    #     excluded_manager = self.excluded_manager
+    #
+    #     regular_instance = regular_manager(
+    #         data={
+    #             'name': 'Jon Snow',
+    #             'extra_required_field': self.required_model.pk,
+    #         },
+    #     )
+    #
+    #     self.assertTrue(regular_instance.is_valid())
+    #
+    #     excluded_manager(
+    #         data={
+    #             'name': 'Jon Snow',
+    #         },
+    #         extra_required_field=self.required_model,
+    #     )
+    #
+    #     with self.assertRaises(TypeError):
+    #         excluded_manager(
+    #             data={
+    #                 'name': 'Jon Snow',
+    #             },
+    #             extra_required_field=self.required_model.pk,
+    #         )
