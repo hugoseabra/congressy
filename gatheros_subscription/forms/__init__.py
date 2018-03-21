e36a7b73f69c03fd3043f436715fa47783fa68a3
Nathan Martins <nathan.eua@gmail.com>
2018-03-21 20:07:53 -0300

    Survey: Implementando os forms de aplicação do cliente.

--- gatheros_subscription/forms/__init__.py
+++ gatheros_subscription/forms/__init__.py
@@ -3,3 +3,4 @@ from .form_config import FormConfigForm
 from .lot import LotForm
 from .subscription import SubscriptionForm
 from .export import SubscriptionFilterForm
+from .survey import SurveyForm
---
+++ gatheros_subscription/forms/survey.py
@@ -0,0 +1,31 @@
+""" Formulários de `Questionários/Surveys` """
+from django import forms
+
+from survey.services import SurveyService
+from gatheros_subscription.models import EventSurvey
+
+
+class SurveyForm(forms.Form):
+    """ Formulário de survey. """
+
+    def __init__(self, event, **kwargs):
+        self.event = event
+        super().__init__(**kwargs)
+        self.service = SurveyService(**kwargs)
+        self.fields.update(self.service.fields)
+
+        self.fields['name'].help_text = 'Nome do seu questionário'
+        self.fields['description'].help_text = 'Uma descrição para te ajudar' \
+                                               ' a identificar este ' \
+                                               'questionário. '
+
+    def is_valid(self):
+        return super().is_valid() and self.service.is_valid()
+
+    def save(self):
+        survey = self.service.save()
+        EventSurvey.objects.create(
+            survey=survey,
+            event=self.event
+        )
+        return survey
