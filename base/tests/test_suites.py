6b4ef38f8d360cd99d99a6eecbefddc1270bbb94
Hugo Seabra <hugoseabra19@gmail.com>
2018-03-29 20:13:46 -0300

    Melhoria: test_suite testando criação e edição de manager e application services

--- base/tests/test_suites.py
+++ base/tests/test_suites.py
@@ -3,9 +3,10 @@ from test_plus.test import TestCase
 
 
 class ManagerPersistenceTestCase(TestCase):
-    """ Testes de persistência de dados: criação e edição. """
+    """ Testes de persistência de dados de manager: criação e edição. """
     manager_class = None
     """
+    :type: Manager
     Classe do Manager a ser testada.
     """
 
@@ -22,10 +23,16 @@ class ManagerPersistenceTestCase(TestCase):
     """
 
     required_fieds = ()
-    """ Campos obrigatórios a serem mantidos na edição. """
+    """
+    :type: tuple
+    Campos obrigatórios a serem mantidos na edição.
+    """
 
     data_edit_to = {}
-    """ Campos a serem alterados na edição. """
+    """
+    :type: dict
+    Campos a serem alterados na edição.
+    """
 
     def _create_manager(self, instance=None, data=None):
         if not data:
@@ -93,3 +100,111 @@ class ManagerPersistenceTestCase(TestCase):
     def test_edit(self):
         """ Testa edição. """
         pass
+
+
+class ApplicationServicePersistenceTestCase(TestCase):
+    """ Testes de persistência de dados de application service: criação e
+    edição.
+    """
+    application_service_class = None
+    """
+    :type: ApplicationService
+    Classe do Application a ser testada.
+    """
+
+    data = None
+    """
+    :type: dict
+    Dados a serem enviados para criação e edição:
+    Criação:
+        - ele usará exatamente o que for informad.
+    Edição:
+        - o valores dos campos obrigatórios serão mantidos (informados em
+         "required_fields") e valores informados em "data_edit_to" serão
+         alterados.
+    """
+
+    required_fieds = ()
+    """
+    :type: tuple
+    Campos obrigatórios a serem mantidos na edição.
+    """
+
+    data_edit_to = {}
+    """
+    :type: dict
+    Campos a serem alterados na edição.
+    """
+
+    def _create_service(self, instance=None, data=None):
+        if not data:
+            data = self.data
+
+        if instance is not None:
+            service = self.application_service_class(
+                instance=instance,
+                data=data
+            )
+        else:
+            service = self.application_service_class(data=data)
+
+        return service
+
+    def _create_instance(self, service=None):
+        if not service:
+            service = self._create_service()
+
+        valid = service.is_valid()
+
+        if not valid:
+            self.fail(service.errors)
+
+        return service.save()
+
+    def create(self):
+        """ Cria uma instância model ligado ao manager """
+        service = self._create_service()
+        instance = self._create_instance(service)
+        self.assertIsInstance(instance, service.manager_class.Meta.model)
+        return instance
+
+    def edit(self):
+        """ Edita dados de um model ligado ao manager """
+        instance = self._create_instance()
+
+        data = {}
+        for field_name in self.required_fieds:
+            if field_name not in self.data:
+                self.fail(
+                    'O campo "{}" em "required_fields" não existe em'
+                    ' "data".'.format(field_name)
+                )
+            data[field_name] = self.data.get(field_name)
+
+        data.update(self.data_edit_to)
+
+        service = self._create_service(instance=instance, data=data)
+
+        valid = service.is_valid()
+
+        if not valid:
+            self.fail(service.errors)
+
+        instance = service.save()
+        self.assertIsInstance(instance, service.manager_class.Meta.model)
+
+        for field_name, value in self.data_edit_to.items():
+            self.assertEqual(getattr(instance, field_name), value)
+
+    @abstractmethod
+    def test_create(self):
+        """ Testa criação. """
+        pass
+
+    @abstractmethod
+    def test_edit(self):
+        """ Testa edição. """
+        pass
+
+
+
