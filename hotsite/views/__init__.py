0cf25c7c5374cba6273fb920b02fa6d73d43c648
Nathan S. Martins <nathan.eua@gmail.com>
2018-04-18 10:36:59 -0300

    Removing old custom wizard form things.

--- hotsite/views/__init__.py
+++ hotsite/views/__init__.py
@@ -2,8 +2,5 @@ from .mixins import EventMixin, SubscriptionFormMixin
 from .hotsite import HotsiteView
 from .coupon import CouponView
 from .subscription import SubscriptionView
-from .subscription_form_steps import StepOne, StepTwo, StepFive
 from .subscription_form_wizard import SubscriptionWizardView
-from .subscription_form_bootstrappers import LotBootstrapper, \
-    PersonBootstrapper
 from .subscription_status_view import SubscriptionStatusView
