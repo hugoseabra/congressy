8412b01dcc31e029230fd324fd27a3dea464bcab
Nathan S. Martins <nathan.eua@gmail.com>
2018-04-18 10:32:16 -0300

    Simple (by_lot, no payments, no survey's) subscriptions working.

--- hotsite/views/subscription_form_wizard.py
+++ hotsite/views/subscription_form_wizard.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from formtools.wizard.views import SessionWizardView
+from django.contrib import messages
 from django.http import HttpResponseRedirect
-from hotsite.views.mixins import EventMixin
-from hotsite import forms
-from gatheros_subscription.models import Lot
+from django.urls import reverse_lazy
+from formtools.wizard.views import SessionWizardView
 
+from gatheros_subscription.models import Lot, Subscription
+from hotsite import forms
+from hotsite.views.mixins import EventMixin
 
 FORMS = [("lot", forms.LotsForm),
          ("person", forms.SubscriptionPersonForm),
@@ -32,7 +33,6 @@ def is_paid_lot(wizard):
 
 
 class SubscriptionWizardView(EventMixin, SessionWizardView):
-
     condition_dict = {'payment': is_paid_lot}
 
     def get_template_names(self):
@@ -40,30 +40,83 @@ class SubscriptionWizardView(EventMixin, SessionWizardView):
 
     def done(self, form_list, **kwargs):
 
-        # Process non-payment subscriptions here:
-        # We have a person in storage.
-        # We have the lot data in storage.
-        # Survey has already been processed.
+        """
+          Process non-payment subscriptions here:
+            - We have a person in storage.
+            - We have the lot data in storage.
+            - Survey has already been processed.
+        """
 
+        # If we already have a subscription in storage, if this is the case,
+        # then it was created by the payment step.
+        if not hasattr(self.storage, 'subscription'):
 
-        raise Exception('fuck me its 02:04 AM')
+            lot_data = self.storage.get_step_data('lot')
+            lot = lot_data.get('lot-lots', '')
+
+            # Get a lot object.
+            if not isinstance(lot, Lot):
+                try:
+                    lot = Lot.objects.get(pk=lot, event=self.event)
+                except Lot.DoesNotExist:
+                    message = 'Não foi possivel resgatar um Lote ' \
+                              'a partir das referencias: lot<{}> e evento<{}>.' \
+                        .format(lot, self.event)
+                    raise TypeError(message)
+
+            subscription = None
+
+            try:
+                subscription = Subscription.objects.get(
+                    person=self.storage.person,
+                    event=self.event
+                )
+            except Subscription.DoesNotExist:
+                subscription = Subscription(
+                    person=self.storage.person,
+                    event=self.event,
+                    created_by=self.request.user.id
+                )
+
+            # Insere ou edita lote
+            subscription.lot = lot
+            subscription.save()
+            self.storage.subscription = subscription
+
+        subscription = self.storage.subscription
+
+        messages.success(
+            self.request,
+            'Inscrição realizada com sucesso!'
+        )
+
+        if subscription.lot.price and subscription.lot.price > 0:
+            return HttpResponseRedirect(reverse_lazy(
+                'public:hotsite-subscription-status', kwargs={
+                    'slug': self.event.slug,
+                }))
+
+        return HttpResponseRedirect(reverse_lazy(
+                'public:hotsite', kwargs={
+                    'slug': self.event.slug,
+                }))
 
     # this runs for the step it's on as well as for the step before
     def get_form_initial(self, step):
 
-            if step == 'lot':
-                return self.initial_dict.get(step, {'event': self.event})
+        if step == 'lot':
+            return self.initial_dict.get(step, {'event': self.event})
 
-            # get the data for step person from  step lot
-            if step == 'person':
-                prev_data = self.storage.get_step_data('lot')
-                lot = prev_data.get('lot-lots', '')
-                return self.initial_dict.get(step, {
-                    'lot': lot,
-                    'event': self.event,
-                })
+        # get the data for step person from  step lot
+        if step == 'person':
+            prev_data = self.storage.get_step_data('lot')
+            lot = prev_data.get('lot-lots', '')
+            return self.initial_dict.get(step, {
+                'lot': lot,
+                'event': self.event,
+            })
 
-            return self.initial_dict.get(step, {})
+        return self.initial_dict.get(step, {})
 
     def process_step(self, form):
 
@@ -80,126 +133,3 @@ class SubscriptionWizardView(EventMixin, SessionWizardView):
         return form_data
 
 
-# class SubscriptionFormWizard(FormWizard):
-#     """
-#         View responsavel por decidir onde se inicia o process de inscrição
-#     """
-#
-#     wizard_steps = {
-#         1: StepOne,
-#         2: StepTwo,
-#         5: StepFive,
-#     }
-#
-#     persisting_steps = [StepTwo, StepFive]
-#
-#     event = None
-#
-#     def dispatch(self, request, *args, **kwargs):
-#         pre_flight = self.pre_flight(request=request)
-#
-#         if pre_flight:
-#             return pre_flight
-#
-#         response = super().dispatch(request, *args, **kwargs)
-#         return response
-#
-#     def get_context_data(self, step, **kwargs):
-#
-#         form_wizard_context = super().get_context_data(**kwargs)
-#         step_context = step.get_context_data(**kwargs)
-#
-#         # Merging contexts
-#         context = {**form_wizard_context, **step_context}
-#
-#         return context
-#
-#     def get(self, request, *args, **kwargs):
-#
-#         """
-#             Se for evento com inscrição por lotes, ir para o step 1,
-#             caso contrario pode pular direto para o step 2.
-#         """
-#         if self.event.subscription_type == Event.SUBSCRIPTION_BY_LOTS:
-#             step_1 = StepOne()
-#
-#             return render(request=request, template_name=step_1.template_name,
-#                           context=step_1.get_step_context_data(
-#                               event=self.event))
-#         else:
-#             raise NotImplementedError('error')
-#
-#     def subscription_enabled(self):
-#
-#         lots = self.get_lots()
-#         if len(lots) == 0:
-#             return False
-#
-#         return self.event.status == Event.EVENT_STATUS_NOT_STARTED
-#
-#     def get_lots(self):
-#         return self.event.lots.filter(private=False)
-#
-#     def post(self, request, *args, **kwargs):
-#
-#         pre_flight = self.pre_flight(request=request)
-#
-#         if pre_flight:
-#             return pre_flight
-#
-#         post_data = request.POST.copy()
-#
-#         super().post(request, *args, **kwargs)
-#         self.current_step(dirty_antecessor_data=post_data, event=self.event)
-#
-#         form_class = self.current_step.form_class
-#
-#         current_form = form_class(data=post_data,
-#                                   dependencies=self.current_step.dependency_artifacts)
-#
-#         if current_form.is_valid():
-#
-#             if self.current_step in self.persisting_steps:
-#                 current_form.save()
-#
-#             if self.next_step:
-#
-#                 next_step = self.next_step(
-#                     validated_antecessor_form=current_form,
-#                     event=self.event)
-#
-#                 if self.next_step == StepFive:
-#
-#
-#
-#                     return redirect(next_step.redirect_url)
-#
-#                 return render(request=request,
-#                               template_name=next_step.template_name,
-#                               context=next_step.get_step_context_data(
-#                                   event=self.event))
-#
-#         messages.error(request, 'Por favor corrigir os erros abaixo.')
-#         return render(request=request,
-#                       template_name=self.current_step.template_name,
-#                       context=self.current_step.get_step_context_data(
-#                           event=self.event, form=current_form))
-#
-#     def pre_flight(self, request):
-#
-#         slug = self.kwargs.get('slug')
-#
-#         if not slug:
-#             return redirect('https://congressy.com')
-#
-#         self.event = get_object_or_404(Event, slug=slug)
-#
-#         if not request.user.is_authenticated:
-#             return redirect('public:hotsite', slug=self.event.slug)
-#
-#         enabled = self.subscription_enabled()
-#
-#         if not enabled:
-#             return redirect('public:hotsite', slug=self.event.slug)
-#
-#         return None
