e30d68f521232cffdb0de128d8d9db102b68d974
Nathan Martins <nathan.eua@gmail.com>
2018-04-22 02:44:35 -0300

    Addons: Implementado tests de preço e de temas

--- addon/tests/mock_factory.py
+++ addon/tests/mock_factory.py
@@ -2,10 +2,12 @@
     Mock factory used during tests to create required objects
 """
 
+from datetime import datetime, timedelta
+
 from faker import Faker
 
-from associate.models import Associate
-from gatheros_event.models import Organization
+from gatheros_event.models import Event, Organization, Category
+from gatheros_subscription.models import LotCategory
 
 
 class MockFactory:
@@ -16,4 +18,29 @@ class MockFactory:
     def __init__(self):
         self.fake_factory = Faker()
 
+    def fake_lot_category(self, event=None):
+        if not event:
+            event = self.fake_event()
 
+        return LotCategory.objects.create(event=event,
+                                          name=self.fake_factory.words(
+                                              nb=3, ext_word_list=None),
+                                          description=self.fake_factory.words(
+                                              nb=7, ext_word_list=None)
+                                          )
+
+    def fake_organization(self):
+        return Organization.objects.create(name=self.fake_factory.company())
+
+    def fake_event(self, organization=None):
+
+        if not organization:
+            organization = self.fake_organization()
+
+        return Event.objects.create(
+            organization=organization,
+            name='Event: ' + ' '.join(self.fake_factory.words(nb=3)),
+            date_start=datetime.now() + timedelta(days=3),
+            date_end=datetime.now() + timedelta(days=4),
+            category=Category.objects.first(),
+        )
--- addon/tests/test_managers.py
+++ addon/tests/test_managers.py
@@ -1,7 +1,12 @@
 """ Testes de managers do módulo de opcionais. """
 
+import decimal
+
+import random
+
 from addon import managers
 from base.tests.test_suites import ManagerPersistenceTestCase
+from .mock_factory import MockFactory
 
 
 class ThemeManagerPersistenceTest(ManagerPersistenceTestCase):
@@ -15,6 +20,47 @@ class ThemeManagerPersistenceTest(ManagerPersistenceTestCase):
     def setUp(self):
         self.data = {
             'name': 'my name',
+        }
+
+    def test_create(self):
+        self.create()
+
+    def test_edit(self):
+        self.edit()
+
+
+class PriceManagerPersistenceTest(ManagerPersistenceTestCase):
+    """ Testes de persistência de dados: criação e edição."""
+    manager_class = managers.PriceManager
+    required_fields = ('price', 'lot_category', 'release_days')
+
+    data_edit_to = {
+        'release_days': 42,
+        'price': decimal.Decimal(random.randrange(155, 389)) / 100,
+    }
+
+    def _create_manager(self, instance=None, data=None):
+
+        if not data:
+            data = self.data
+
+        if 'lot_category' not in data:
+            self.data['lot_category'] = self.fake_factory.fake_lot_category()
+
+        if instance is not None:
+            manager = self.manager_class(instance=instance, data=data)
+        else:
+            manager = self.manager_class(data=data)
+
+        return manager
+
+    def setUp(self):
+
+        self.fake_factory = MockFactory()
+        self.data = {
+            'release_days': 1,
+            'price': decimal.Decimal(random.randrange(155, 389)) / 100,
+            'lot_category': self.fake_factory.fake_lot_category().pk,
         }
 
     def test_create(self):
