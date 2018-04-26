62d08cb5c6f5b2ccb011927bf9ef2b7813ad667c
Nathan Martins <nathan.eua@gmail.com>
2018-04-26 14:49:25 -0300

    Hotfix: correção do bug d form

--- hotsite/forms/lots.py
+++ hotsite/forms/lots.py
@@ -11,12 +11,23 @@ from gatheros_subscription.models import Lot
 
 
 class LotFormModelChoiceField(ModelChoiceField):
-    def label_from_instance(self, obj):
 
+    def label_from_instance(self, obj):
         if obj.price and obj.price > 0:
             return "{} - R${}".format(obj.name, obj.get_calculated_price())
 
         return "{}".format(obj.name)
+
+    def to_python(self, value):
+
+        event = self.queryset.first().event
+
+        self.queryset = Lot.objects.filter(
+            event=event,
+            date_start__lte=datetime.now(),
+            date_end__gte=datetime.now())
+
+        return super().to_python(value)
 
 
 class LotsForm(forms.Form):
@@ -34,8 +45,3 @@ class LotsForm(forms.Form):
             required=True,
             label='lote',
         )
-
-        if kwargs.get('instance'):
-            self.fields['lots'].queryset = Lot.objects.filter(event=self.event,
-                                                              date_start__lte=datetime.now(),
-                                                              date_end__gte=datetime.now())
