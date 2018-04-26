ba749645b9c4e66156dfe68afa7045941cccec16
Nathan Martins <nathan.eua@gmail.com>
2018-04-26 16:48:23 -0300

    Addons: Implementando serializers basicos e parcialmente implementando         viewsets.

---
+++ addon/viewsets.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets
+from rest_framework.authentication import (
+    BasicAuthentication,
+    SessionAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
+
+from addon import models, serializers
+
+
+class RestrictionViewMixin(object):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+
+class ProductViewSet(RestrictionViewMixin, viewsets.ModelViewSet):
+    """
+        API endpoint that allows users to be viewed or edited.
+    """
+    queryset = models.Product.objects.all().order_by('name')
+    serializer_class = serializers.ProductSerializer
+
+
+class ServiceViewSet(RestrictionViewMixin, viewsets.ModelViewSet):
+    """
+        API endpoint that allows users to be viewed or edited.
+    """
+    queryset = models.Service.objects.all().order_by('name')
+    serializer_class = serializers.Ser
