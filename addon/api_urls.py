514131d9c2a7f773f9568e60d33686e5c9e121e5
Nathan Martins <nathan.eua@gmail.com>
2018-04-27 12:03:57 -0300

    Addons: Endpoints do Hotsite

--- addon/api_urls.py
+++ addon/api_urls.py
@@ -7,5 +7,11 @@ router = routers.DefaultRouter()
 
 router.register(r'addon/optionals/services', viewsets.ServiceViewSet)
 router.register(r'addon/optionals/products', viewsets.ProductViewSet)
+router.register(r'addon/subscriptions/(?P<subscription_pk>['
+                r'0-9A-Fa-f-]+)/optionals/services',
+                viewsets.SubscriptionServiceViewSet)
+router.register(r'addon/subscriptions/(?P<subscription_pk>['
+                r'0-9A-Fa-f-]+)/optionals/products',
+                viewsets.SubscriptionProductViewSet)
 
 urlpatterns = router.urls
