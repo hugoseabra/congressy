560e5ab5c0222c9133151805deae992d2bab79ff
Hugo Seabra <hugoseabra19@gmail.com>
2018-04-30 11:42:30 -0300

    Addon: fixtures e ajustes simples.

--- project/manage/settings/__init__.py
+++ project/manage/settings/__init__.py
@@ -39,6 +39,7 @@ FIXTURE_DIRS += [
     os.path.join(BASE_DIR, 'gatheros_event', 'tests', 'fixtures'),
     os.path.join(BASE_DIR, 'gatheros_subscription', 'tests', 'fixtures'),
     os.path.join(BASE_DIR, 'payment', 'tests', 'fixtures'),
+    os.path.join(BASE_DIR, 'addon', 'tests', 'fixtures'),
 ]
 # ============================= TEMPLATES =================================== #
 TEMPLATES[0]['OPTIONS']['context_processors'].append(
