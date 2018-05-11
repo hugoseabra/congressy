40ec10ab9a1561a18650b01d44f834de149ea851
Nathan Martins <nathan.eua@gmail.com>
2018-05-11 15:49:43 -0300

    scientific_work: url

--- scientific_work/views.py
+++ scientific_work/views.py
@@ -9,7 +9,7 @@ from .forms import NewWorkForm
 class WorkAddFormView(AccountMixin, FormView):
     template_name = "scientific_work/work_form.html"
     form_class = NewWorkForm
-    success_url = reverse_lazy('front:start')
+    success_url = reverse_lazy('scientific_work:work-add')
 
     def form_valid(self, form):
         messages.success(self.request, 'Submissão realizado com sucesso.')
