712541298e094ce03e4cabf4c95b4968179aa569
Hugo Seabra <hugoseabra19@gmail.com>
2018-06-01 23:51:06 -0300

    Payment: removendo model Provider e transferindo Payment de payment_debt.  PaymentDebt: registro de pendência é único para um determinado tipo em uma inscrição.

--- payment_debt/models.py
+++ payment_debt/models.py
@@ -22,7 +22,7 @@ class Debt(models.Model):
         verbose_name = 'pendência'
         verbose_name_plural = 'pendências'
         unique_together = (
-            ('lot', 'subscription'),
+            ('type', 'subscription'),
         )
 
     DEBT_TYPE_SUBSCRIPTION = 'subscription'
@@ -43,13 +43,6 @@ class Debt(models.Model):
         (DEBT_STATUS_DEBT, 'pendente'),
         (DEBT_STATUS_PAID, 'pago'),
         (DEBT_STATUS_CREDIT, 'com crédito'),
-    )
-
-    lot = models.ForeignKey(
-        Lot,
-        on_delete=models.PROTECT,
-        related_name='debts',
-        editable=False,
     )
 
     subscription = models.ForeignKey(
@@ -121,11 +114,13 @@ class Debt(models.Model):
             self.get_status_display(),
         )
 
-    def save(self, *args, **kwargs):
-        if self._state.adding is True:
-            self.lot = self.subscription.lot
+    @property
+    def paid(self):
+        return self.status == self.DEBT_STATUS_PAID
 
-        super().save(*args, **kwargs)
+    @property
+    def has_credit(self):
+        return self.status == self.DEBT_STATUS_CREDIT
 
 
 class DebtConfig(models.Model):
@@ -178,7 +173,7 @@ def _debt_create_config_post_save(instance, created, raw, **_):
     if raw is True or created is False:
         return
 
-    lot = instance.lot
+    lot = instance.subscription.lot
 
     DebtConfig.objects.create(
         debt=instance,
@@ -187,92 +182,3 @@ def _debt_create_config_post_save(instance, created, raw, **_):
         total_installments=lot.installment_limit,
         free_installments=lot.num_install_interest_absortion,
     )
-
-
-class Payment(models.Model):
-    """
-    Registro de pagamento para uma determinada inscrição enquanto ela estava
-    em um determinado lote.
-    """
-
-    class Meta:
-        verbose_name = 'pagamento'
-        verbose_name_plural = 'pagamentos'
-
-    CASH_TYPE_BOLETO = 'boleto'
-    CASH_TYPE_CREDIT_CARD = 'credit_card'
-    CASH_TYPE_DEBIT_CARD = 'debit_card'
-    CASH_TYPE_PAYCHECK = 'paycheck'
-    CASH_TYPE_BANK_DEPOSIT = 'bank_deposit'
-    CASH_TYPE_MONEY = 'money'
-    CASH_TYPE_TRANSFER = 'bank_transfer'
-
-    CASH_TYPES = (
-        (CASH_TYPE_BOLETO, 'Boleto'),
-        (CASH_TYPE_CREDIT_CARD, 'Cartão de Crédito'),
-        (CASH_TYPE_DEBIT_CARD, 'Cartão de Débito'),
-        (CASH_TYPE_PAYCHECK, 'Cheque'),
-        (CASH_TYPE_BANK_DEPOSIT, 'Depósito'),
-        (CASH_TYPE_MONEY, 'Dinheiro'),
-        (CASH_TYPE_TRANSFER, 'Transferência Bancária'),
-    )
-
-    lot = models.ForeignKey(
-        Lot,
-        on_delete=models.PROTECT,
-        related_name='payments',
-        editable=False,
-    )
-
-    subscription = models.ForeignKey(
-        Subscription,
-        on_delete=models.CASCADE,
-        related_name='payments'
-    )
-
-    cash_type = models.CharField(
-        max_length=30,
-        choices=CASH_TYPES,
-    )
-
-    manual = models.BooleanField(
-        default=False,
-        verbose_name='lançamento manual',
-    )
-
-    manual_author = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True,
-        verbose_name='author do lançamento manual',
-    )
-
-    amount = models.DecimalField(
-        decimal_places=2,
-        max_digits=11,
-        null=True,
-        blank=True,
-        verbose_name='valor',
-        help_text='valor cobrado ao comprador',
-    )
-
-    paid = models.BooleanField(
-        default=False,
-        verbose_name='pago',
-    )
-
-    def __str__(self):
-        string = '{} - $ {}'.format(
-            self.get_cash_type_display(),
-            self.amount,
-        )
-        if self.manual_author:
-            string += '(manual: {})'.format(self.manual_author)
-
-        return string
-
-    def save(self, *args, **kwargs):
-        if self._state.adding is True:
-            self.lot = self.subscription.lot
-
-        return super().save(*args, **kwargs)
