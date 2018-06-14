951912097c70337b09ebd4ce51c01a55d0e469fc
Nathan Martins <nathan.eua@gmail.com>
2018-06-14 14:06:39 -0300

    Não permitir quebra de linha no conteúdo do certificado no backend

--- certificate/forms/certificate.py
+++ certificate/forms/certificate.py
@@ -35,34 +35,41 @@ class CertificatePartialForm(forms.ModelForm):
         except ObjectDoesNotExist:
             pass
 
+    def clear_background_image(self):
+        """ Limpa campo `image4` """
+        self._clear_file('background_image')
+        return self.cleaned_data['background_image']
 
-def clear_background_image(self):
-    """ Limpa campo `image4` """
-    self._clear_file('background_image')
-    return self.cleaned_data['background_image']
+    def _clear_file(self, field_name):
+        """Removes files from model"""
 
+        if field_name not in self.changed_data:
+            return
 
-def _clear_file(self, field_name):
-    """Removes files from model"""
+        file = getattr(self.instance, field_name)
+        if not file:
+            return
 
-    if field_name not in self.changed_data:
-        return
+        storage = file.storage
+        path = file.path
+        storage.delete(path)
 
-    file = getattr(self.instance, field_name)
-    if not file:
-        return
+        storage = file.default.storage
+        path = file.default.path
+        storage.delete(path)
 
-    storage = file.storage
-    path = file.path
-    storage.delete(path)
+        storage = file.thumbnail.storage
+        path = file.thumbnail.path
+        storage.delete(path)
 
-    storage = file.default.storage
-    path = file.default.path
-    storage.delete(path)
+    def clean_text_content(self):
+        text_content = self.cleaned_data['text_content']
+        text_content = text_content\
+            .replace('\r\n', ' ')\
+            .replace('\r', ' ')\
+            .replace('\n', ' ')
 
-    storage = file.thumbnail.storage
-    path = file.thumbnail.path
-    storage.delete(path)
+        return text_content
 
 
 class CertificateForm(forms.ModelForm):
