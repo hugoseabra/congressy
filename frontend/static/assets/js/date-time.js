1feef981e7eefd9568267305e7aa8f0047f96e4f
gabriel <gabrielrumiranda@gmail.com>
2018-06-14 13:02:17 -0300

    Finalização da criação do calendário

--- frontend/static/assets/js/date-time.js
+++ frontend/static/assets/js/date-time.js
@@ -4,45 +4,73 @@
 // #datepicker-begin .datapicker
 // #datepicker-end  .datapicker
 
+var minDate = new Date();
+    minDate.setHours(0, 0, 0, 0);
+
+var tooltips = {
+    today: 'Ir para hoje',
+    clear: 'Limpar seleção',
+    close: 'Feche o seletor',
+    selectMonth: 'Selecione o mês',
+    prevMonth: 'Mês anterior',
+    nextMonth: 'Próximo mês',
+    selectYear: 'Selecione o ano',
+    prevYear: 'Ano anterior',
+    nextYear: 'Próximo ano',
+    selectDecade: 'Selecione Década',
+    prevDecade: 'Década anterior',
+    nextDecade: 'Próxima Década',
+    prevCentury: 'Século Anterior',
+    nextCentury: 'Próximo Século',
+    incrementHour: 'Incremente a hora',
+    pickHour: 'Selecione a Hora',
+    decrementHour: 'Decremente a Hora',
+    incrementMinute: 'Incremente a Hora',
+    pickMinute: 'Selecione o Minuto',
+    decrementMinute: 'Decremente o Minuto',
+    incrementSecond: 'Incremente o Segundo',
+    pickSecond: 'Selecione o Segundo',
+    decrementSecond: 'Decremente o Segundo'
+};
+
 function createDateStartDateEnd(path_start, path_end) {
 
-    $(path_start).datetimepicker({
-        locale: 'pt-br',
-        format: 'DD/MM/YYYY',
-        allowInputToggle: true,
-        defaultDate: new Date(),
-        minDate: new Date()
+    var date_end = createDatePicker(path_end);
 
-    }).on('dp.change', function (e) {
-        $(path_end).data("DateTimePicker").minDate(e.date)
-    });
-
-    $(path_end).datetimepicker({
-        locale: 'pt-br',
-        format: 'DD/MM/YYYY',
-        allowInputToggle: true,
-        defaultDate: new Date(),
-        minDate: new Date()
+    var date_start = createDatePicker(path_start).on('dp.change', function (e) {
+        date_end.data("DateTimePicker").minDate(e.date);
     });
 
 }
 
-function createTimePicker() {
-    $('.timepicker').datetimepicker({
+function createTimePicker(el) {
+    if (!el) {
+        el = '.timepicker';
+    }
+    el = $(el);
+    el.datetimepicker({
         locale: 'pt-br',
         format: 'HH:mm',
         allowInputToggle: true,
-        defaultDate: new Date(),
-        minDate: new Date()
+        tooltips: tooltips,
     });
+    return el
 }
 
-function createDatePicker() {
-        $('.datapicker').datetimepicker({
+function createDatePicker(el) {
+    if (!el) {
+        el = '.datapicker';
+    }
+    el = $(el);
+    el.datetimepicker({
         locale: 'pt-br',
         format: 'DD/MM/YYYY',
         allowInputToggle: true,
-        defaultDate: new Date(),
-        minDate: new Date()
+        minDate: minDate,
+        tooltips: tooltips,
+        showTodayButton: true
+
     });
+
+    return el;
 }
