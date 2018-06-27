139224ed9e9225a746e82959c73c4461fe2e61c3
gabriel <gabrielrumiranda@gmail.com>
2018-06-27 14:20:44 -0300

    Ajustes nos forms

--- frontend/static/assets/js/user_notification.js
+++ frontend/static/assets/js/user_notification.js
@@ -1,14 +1,20 @@
- $(":input").change(function () { //trigers change in all input fields including text type
+$(document).ready(function () {
+    var submitButton = $('form').find(':submit');
+    submitButton.prop('disabled', true);
+
+    $("form").on("submit", function (e) {
+        $(window).off("beforeunload");
+        var submitButton = $('form').find(':submit');
+        submitButton.prop('disabled', true);
+    });
+
+    $(":input").change(function () {//trigers change in all input fields including text type
+        var submitButton = $('form').find(':submit');
+        submitButton.prop('disabled', false);
         $(window).on("beforeunload", function () {
             return "Você tem certeza? Você não terminou de preencher o formulário!";
         });
     });
 
 
-
-
-$(document).ready(function () {
-    $("form").on("submit", function (e) {
-        $(window).off("beforeunload");
-    });
 });
