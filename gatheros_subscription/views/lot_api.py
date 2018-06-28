8e450a2c727c69e2b2503aa8d6b75f352a197e19
Nathan Martins <nathan.eua@gmail.com>
2018-06-28 17:37:45 -0300

    Implementando frontend.

--- gatheros_subscription/views/lot_api.py
+++ gatheros_subscription/views/lot_api.py
@@ -22,14 +22,17 @@ class LotChangeSurveyAPIView(generics.RetrieveUpdateAPIView):
     def dispatch(self, request, *args, **kwargs):
         event_pk = kwargs.pop('event_pk')
         lot_pk = kwargs.get('lot_pk')
-        survey_pk = kwargs.get('survey_pk')
+        survey_pk = kwargs.get('survey_pk', False)
 
-        if event_pk is None or lot_pk is None or survey_pk is None:
+        if event_pk is None or lot_pk is None:
             return Response(status=404)
 
         self.event = get_object_or_404(Event, pk=event_pk)
         self.lot = get_object_or_404(Lot, pk=lot_pk, event=self.event.pk)
-        self.survey = get_object_or_404(Survey, pk=survey_pk,)
+
+        if survey_pk:
+            self.survey = get_object_or_404(Survey, pk=survey_pk,)
+
         return super().dispatch(request, *args, **kwargs)
 
     def get_object(self):
@@ -43,19 +46,22 @@ class LotChangeSurveyAPIView(generics.RetrieveUpdateAPIView):
         instance = self.get_object()
 
         # Fetch for existing event_survey or create a new one.
-        try:
-            event_survey = EventSurvey.objects.get(
-                survey_id=self.survey.pk,
-            )
-            event_survey.event = self.event
-            event_survey.save()
-        except EventSurvey.DoesNotExist:
-            event_survey = EventSurvey.objects.create(
-                event=self.event,
-                survey=self.survey,
-            )
+        if self.survey:
+            try:
+                event_survey = EventSurvey.objects.get(
+                    survey_id=self.survey.pk,
+                )
+                event_survey.event = self.event
+                event_survey.save()
+            except EventSurvey.DoesNotExist:
+                event_survey = EventSurvey.objects.create(
+                    event=self.event,
+                    survey=self.survey,
+                )
 
-        request.data['event_survey'] = event_survey.pk
+            request.data['event_survey'] = event_survey.pk
+        else:
+            request.data['event_survey'] = ''
 
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
