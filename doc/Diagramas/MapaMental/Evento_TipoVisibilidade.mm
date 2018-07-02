559470adc206304a430f87f7db5af4b2c59cf793
Hugo Seabra <hugoseabra19@gmail.com>
2018-07-02 11:23:05 -0300

    DOC: mapas mentais de novas estruturas de estados de eventos

---
+++ doc/Diagramas/MapaMental/Evento_TipoVisibilidade.mm
@@ -0,0 +1,61 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1530291102120" ID="ID_1489030663" MODIFIED="1530291244875">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Evento -
+    </p>
+    <p style="text-align: center">
+      Visibilidade
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1530291238969" FOLDED="true" ID="ID_1740948463" MODIFIED="1530299229579" POSITION="right" TEXT="P&#xfa;blico">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1530291257230" FOLDED="true" ID="ID_1473401662" MODIFIED="1530299228069" TEXT="Transi&#xe7;&#xe3;o para privado">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1530291369718" FOLDED="true" ID="ID_1803409263" MODIFIED="1530299227170" TEXT="Configura&#xe7;&#xe3;o de privacidade de lote">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1530293883453" FOLDED="true" ID="ID_1958728498" MODIFIED="1530299225772" TEXT="Ap&#xf3;s salvar configura&#xe7;&#xe3;o de privacidade">
+<node CREATED="1530292276930" ID="ID_1327348025" MODIFIED="1530293914004" TEXT="Verificar se todos os lotes est&#xe3;o privados"/>
+<node CREATED="1530293914413" FOLDED="true" ID="ID_1028329899" MODIFIED="1530299224578" TEXT="Informar que o lote est&#xe1; totalmente privado">
+<node CREATED="1530291396186" FOLDED="true" ID="ID_141577512" MODIFIED="1530299223830" TEXT="o que muda?">
+<node CREATED="1530292462403" ID="ID_773944342" MODIFIED="1530294065386" TEXT="Somente pessoas com cupom podem se cadastrar"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1530291246101" FOLDED="true" ID="ID_1085904270" MODIFIED="1530299999252" POSITION="left" TEXT="Privado">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1530291257230" FOLDED="true" ID="ID_641639670" MODIFIED="1530299996765" TEXT="Transi&#xe7;&#xe3;o para p&#xfa;blico">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1530291369718" FOLDED="true" ID="ID_822531263" MODIFIED="1530299994204" TEXT="Configura&#xe7;&#xe3;o de privacidade de lote">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1530292276930" ID="ID_1651784964" MODIFIED="1530292293695" TEXT="Confirma&#xe7;&#xe3;o de tornar evento &quot;P&#xfa;blico&quot;"/>
+<node CREATED="1530291396186" ID="ID_1169125208" MODIFIED="1530293973256" TEXT="o que muda?">
+<node CREATED="1530292375617" FOLDED="true" ID="ID_1857068234" MODIFIED="1530299992705" TEXT="Gratuito">
+<node CREATED="1530292462403" ID="ID_1423389595" MODIFIED="1530293986040" TEXT="Pessoas sem voucher cupom podem se cadastrar"/>
+</node>
+<node CREATED="1530292374049" FOLDED="true" ID="ID_1967519651" MODIFIED="1530299992039" TEXT="Pago">
+<node CREATED="1530292462403" ID="ID_468256918" MODIFIED="1530293992602" TEXT="Pessoas sem voucher cupom podem se cadastrar"/>
+<node CREATED="1530292626078" FOLDED="true" ID="ID_1372003621" MODIFIED="1530299990520" TEXT="Se n&#xe3;o h&#xe1; alguma inscri&#xe7;&#xe3;o paga">
+<node CREATED="1530292305596" FOLDED="true" ID="ID_698984462" MODIFIED="1530299989536" TEXT="Deve-se publicar inscri&#xe7;&#xf5;es pagas de alguma forma">
+<node CREATED="1530292489209" ID="ID_1104493887" MODIFIED="1530292539898" TEXT="Lote(s) pago(s), e/ou.."/>
+<node CREATED="1530292499310" ID="ID_463990013" MODIFIED="1530292533409" TEXT="Atividade(s) extra(s) paga(s), e/ou..."/>
+<node CREATED="1530292508843" ID="ID_1458100136" MODIFIED="1530292523779" TEXT="Opcional(is) pago(s)."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
