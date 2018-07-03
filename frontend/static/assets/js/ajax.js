69487a294a70ee7f53f12022e11d7ddeddefd95a
Nathan Martins <nathan.eua@gmail.com>
2018-07-03 11:17:26 -0300

    Implmetando um ajaxsender que permita formdata

--- frontend/static/assets/js/ajax.js
+++ frontend/static/assets/js/ajax.js
@@ -6,10 +6,11 @@ window.cgsy = window.cgsy || {};
 
         url = url || window.location.href;
 
-        var success_callback = function () {};
+        var success_callback = function () {
+        };
         var default_fail_callback = function (response) {
             var msg = 'Failure on request to "' + url + '" with method';
-                msg += ' "' + this.method + '".';
+            msg += ' "' + this.method + '".';
 
             if (response.hasOwnProperty('detail')) {
                 msg += ' Detalhes: ' + response.detail;
@@ -45,12 +46,12 @@ window.cgsy = window.cgsy || {};
             return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
         }
 
-        this.setSuccessCallback = function(callback) {
+        this.setSuccessCallback = function (callback) {
             success_callback = callback
         };
 
-        this.setFailCallback = function(callback) {
-            fail_callback = function(response) {
+        this.setFailCallback = function (callback) {
+            fail_callback = function (response) {
                 callback(response);
                 default_fail_callback(response);
             }
@@ -66,6 +67,84 @@ window.cgsy = window.cgsy || {};
                 data: data,
                 encode: true,
                 crossDomain: false,
+                beforeSend: function (xhr, settings) {
+                    if (!csrfSafeMethod(settings.type)) {
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    }
+                },
+                success: success_callback,
+                error: fail_callback
+            });
+        };
+    };
+
+    cgsy.AjaxFormDataSender = function (url) {
+
+        url = url || window.location.href;
+
+        var success_callback = function () {
+        };
+        var default_fail_callback = function (response) {
+            var msg = 'Failure on request to "' + url + '" with method';
+            msg += ' "' + this.method + '".';
+
+            if (response.hasOwnProperty('detail')) {
+                msg += ' Detalhes: ' + response.detail;
+            }
+
+            console.error(msg);
+        };
+
+        var fail_callback = default_fail_callback;
+
+        // CSRF code
+        function getCookie(name) {
+            var cookieValue = null;
+            var i = 0;
+            if (document.cookie && document.cookie !== '') {
+                var cookies = document.cookie.split(';');
+                for (i; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        var csrftoken = getCookie('csrftoken');
+
+        function csrfSafeMethod(method) {
+            // these HTTP methods do not require CSRF protection
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+
+        this.setSuccessCallback = function (callback) {
+            success_callback = callback
+        };
+
+        this.setFailCallback = function (callback) {
+            fail_callback = function (response) {
+                callback(response);
+                default_fail_callback(response);
+            }
+        };
+
+        this.send = function (method, data) {
+
+            data = data || {};
+
+            $.ajax({
+                url: url,
+                type: method,
+                data: data,
+                encode: true,
+                crossDomain: false,
+                processData: false,
+                contentType: false,
                 beforeSend: function (xhr, settings) {
                     if (!csrfSafeMethod(settings.type)) {
                         xhr.setRequestHeader("X-CSRFToken", csrftoken);
