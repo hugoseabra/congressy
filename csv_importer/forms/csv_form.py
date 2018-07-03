2e1d3e6f5ceb2c0dffbc76e2d31e73f75857a99d
Nathan Martins <nathan.eua@gmail.com>
2018-07-03 15:35:42 -0300

    Implementando a passgem do evento via kwargs do form.

--- csv_importer/forms/csv_form.py
+++ csv_importer/forms/csv_form.py
@@ -1,9 +1,27 @@
 from django import forms
 
 from csv_importer.models import CSVImportFile
+from gatheros_event.models import Event
 
 
 class CSVForm(forms.ModelForm):
+    event = None
+
     class Meta:
         model = CSVImportFile
-        fields = '__all__'
+        fields = [
+            'event',
+            'csv_file',
+            'separator',
+            'delimiter',
+            'encoding',
+        ]
+
+        widgets = {
+            'event': forms.HiddenInput(),
+        }
+
+    def __init__(self, event_pk, *args, **kwargs):
+        self.event = Event.objects.get(pk=event_pk)
+        super().__init__(*args, **kwargs)
+        self.fields['event'].inital = self.event.pk
