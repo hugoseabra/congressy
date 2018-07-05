1ca6321f4bd0ad57cd390d72ac3ca1310c8a24f9
Nathan Martins <nathan.eua@gmail.com>
2018-07-05 10:13:46 -0300

    Implementando uma table no frontend e uma view para processar uploads.

--- gatheros_subscription/views/csv_import.py
+++ gatheros_subscription/views/csv_import.py
@@ -1,14 +1,12 @@
 from django.contrib import messages
-from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-
-from csv_importer.forms import CSVForm
-from .subscription import EventViewMixin
+from csv_importer.forms import CSVForm, CSVFileForm
 from csv_importer.models import CSVImportFile
+from .subscription import EventViewMixin
 
 
 class CSVViewMixin(EventViewMixin):
@@ -42,6 +40,11 @@ class CSVFileListView(CSVViewMixin, generic.ListView):
     def get_queryset(self):
         return CSVImportFile.objects.filter(event=self.event)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CSVFileForm(initial={'event': self.event.pk})
+        return context
+
 
 class CSVConfigView(CSVViewMixin, generic.TemplateView):
     template_name = "subscription/csv_import.html"
@@ -53,86 +56,99 @@ class CSVConfigView(CSVViewMixin, generic.TemplateView):
 
 
 class CSVImportView(CSVViewMixin, generic.FormView):
-    form_class = CSVForm
-    preview = False
+    form_class = CSVFileForm
 
-    def dispatch(self, request, *args, **kwargs):
-
-        if not request.is_ajax():
-            message = 'Apenas requisições do tipo XMLHttpRequest são ' \
-                      'permitidas.'
-            return JsonResponse({'error': message}, status=403)
-
-        is_preview = request.POST.get('preview', False)
-
-        if is_preview and is_preview == 'true':
-            self.preview = True
-
-        return super().dispatch(request, *args, **kwargs)
+    def get_success_url(self):  
+        return reverse_lazy('subscription:subscriptions-csv-list', kwargs={
+            'event_pk': self.event.pk
+        })
 
     def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Arquivo submetido com sucesso!")
+        return super().form_valid(form)
 
-        instance = form.save()
-
-        encoding = form.cleaned_data['encoding']
-
-        file_content = instance.csv_file.file.read()
-
-        encoded_content = file_content.decode(encoding)
-
-        content = encoded_content.splitlines()
-
-        delimiter = form.cleaned_data['delimiter']
-        quotechar = form.cleaned_data['separator']
-
-        if self.preview:
-
-            all_lines = content.splitlines()
-
-            first_line = all_lines.pop(0)
-
-            table_keys = first_line.split(delimiter)
-
-            table_heading = ''
-            table_body = ''
-
-            for key in table_keys:
-                table_heading += '<th>' + key + '</th>'
-
-            for line in all_lines:
-
-                current_line = line.split(delimiter)
-
-                table_body += '<tr>'
-
-                for entry in current_line:
-                    entry = entry.strip()
-                    if entry:
-
-                        entry = entry.split(quotechar)
-                        table_body += '<td>'
-
-                        for item in entry:
-                            table_body += item
-
-                        table_body += '</td>'
-                    else:
-                        table_body += '<td> --- </td>'
-
-                table_body += '</tr>'
-
-            html = '<table class="table"><thead><tr>' + \
-                   table_heading + '</tr></thead><tbody>' + table_body + \
-                   '</tbody></table>'
-
-            return HttpResponse(html)
-        else:
-            raise NotImplementedError()
-            # file_obj = ContentFile(, name=instance.csv_file.name)
-            # instance.csv_file = file_obj
-            # instance.save()
-
-            # TODO: Generate a redirect to confirm subscriptions.
-
-    def form_invalid(self, form):
-        return JsonResponse(dict(form.errors.items()))
+# class CSVImportView(CSVViewMixin, generic.FormView):
+#     form_class = CSVForm
+#     preview = False
+#
+#     def dispatch(self, request, *args, **kwargs):
+#
+#         if not request.is_ajax():
+#             message = 'Apenas requisições do tipo XMLHttpRequest são ' \
+#                       'permitidas.'
+#             return JsonResponse({'error': message}, status=403)
+#
+#         is_preview = request.POST.get('preview', False)
+#
+#         if is_preview and is_preview == 'true':
+#             self.preview = True
+#
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def form_valid(self, form):
+#
+#         instance = form.save()
+#
+#         encoding = form.cleaned_data['encoding']
+#
+#         file_content = instance.csv_file.file.read()
+#
+#         encoded_content = file_content.decode(encoding)
+#
+#         content = encoded_content.splitlines()
+#
+#         delimiter = form.cleaned_data['delimiter']
+#         quotechar = form.cleaned_data['separator']
+#
+#         if self.preview:
+#
+#             all_lines = content.splitlines()
+#
+#             first_line = all_lines.pop(0)
+#
+#             table_keys = first_line.split(delimiter)
+#
+#             table_heading = ''
+#             table_body = ''
+#
+#             for key in table_keys:
+#                 table_heading += '<th>' + key + '</th>'
+#
+#             for line in all_lines:
+#
+#                 current_line = line.split(delimiter)
+#
+#                 table_body += '<tr>'
+#
+#                 for entry in current_line:
+#                     entry = entry.strip()
+#                     if entry:
+#
+#                         entry = entry.split(quotechar)
+#                         table_body += '<td>'
+#
+#                         for item in entry:
+#                             table_body += item
+#
+#                         table_body += '</td>'
+#                     else:
+#                         table_body += '<td> --- </td>'
+#
+#                 table_body += '</tr>'
+#
+#             html = '<table class="table"><thead><tr>' + \
+#                    table_heading + '</tr></thead><tbody>' + table_body + \
+#                    '</tbody></table>'
+#
+#             return HttpResponse(html)
+#         else:
+#             raise NotImplementedError()
+#             # file_obj = ContentFile(, name=instance.csv_file.name)
+#             # instance.csv_file = file_obj
+#             # instance.save()
+#
+#             # TODO: Generate a redirect to confirm subscriptions.
+#
+#     def form_invalid(self, form):
+#         return JsonResponse(dict(form.errors.items()))
