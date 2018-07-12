e7a5b6836d994fa9e0ed11432fcc35a9bd869841
Nathan Martins <nathan.eua@gmail.com>
2018-07-12 17:06:22 -0300

    Alterando a view para permitir a criação de PDF's via web service

--- certificate/views/pdf.py
+++ certificate/views/pdf.py
@@ -1,5 +1,15 @@
+import base64
+import json
+import time
+
+import requests
+from django.contrib.sites.models import Site
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template import Template, Context
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views import generic
 from wkhtmltopdf.views import PDFTemplateView
@@ -70,35 +80,42 @@ class CertificatePDFView(AccountMixin, PDFTemplateView):
                self.subscription.attended is True
 
 
-class CertificatePDFExampleView(AccountMixin, PDFTemplateView):
+class CertificatePDFExampleView(AccountMixin, generic.View):
     template_name = 'pdf/certificate.html'
     event = None
-    show_content_in_browser = True
-    permission_denied_url = reverse_lazy('front:start')
     long_name = "Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano"
+    url = 'http://wkhtml.nathanmartins.com:5000'
 
-    cmd_options = {
-        'dpi': 96,
-        'margin-top': 0,
-        'margin-bottom': 0,
-        'margin-left': 0,
-        'margin-right': 0,
-        'page-size': 'A4',
-        'orientation': 'Landscape',
-    }
+    def create_html_string(self):
+        return render_to_string(
+            self.template_name,
+            context=self.get_context_data()
+        )
 
     def pre_dispatch(self, request):
         event_pk = self.kwargs.get('event_pk')
         self.event = get_object_or_404(Event, pk=event_pk)
         return super().pre_dispatch(request)
 
-    def get_context_data(self, **kwargs):
-        context = super(CertificatePDFExampleView, self).get_context_data(
-            **kwargs)
+    def get_context_data(self):
+        context = {}
 
         if self.event.certificate.background_image:
-            image_url = self.event.certificate.background_image.default.url
-            context['background_image'] = image_url
+            image_url = self.event.certificate.background_image.default.path
+
+            with open(image_url, 'rb') as f:
+                encoded_image = base64.b64encode(f.read()).decode()
+            context['background_image'] = encoded_image
+
+        protocol = 'https' if self.request.is_secure() else 'http'
+        site_url = protocol + "://" + Site.objects.get_current().domain
+
+        bootstrap_path = static(
+            'assets/plugins/bootstrap/css/bootstrap.min.css')
+        main_css_path = static('assets/css/main.css')
+
+        context['bootstrap_min_css'] = site_url + bootstrap_path
+        context['main_css'] = site_url + main_css_path
 
         context['event'] = self.event
         context['certificate'] = self.event.certificate
@@ -120,6 +137,43 @@ class CertificatePDFExampleView(AccountMixin, PDFTemplateView):
         )
 
         return text_template.render(context)
+
+    def get(self, request, *args, **kwargs):
+        html = self.create_html_string()
+        epoch = str(int(time.time()))
+        with open('/tmp/wk_' + epoch + '.html', 'w') as f:
+            f.write(html)
+
+        encoded = base64.b64encode(html.encode()).decode()
+
+        data = {
+            'contents': encoded,
+            'options': {
+                'dpi': '96',
+                'margin-top': '0',
+                'margin-left': '0',
+                'margin-right': '0',
+                'margin-bottom': '0',
+                'page-size': 'A4',
+                'orientation': 'Landscape',
+            },
+        }
+
+        headers = {
+            'Content-Type': 'application/json',  # This is important
+        }
+
+        response = requests.post(
+            self.url,
+            data=json.dumps(data),
+            headers=headers,
+        )
+
+        if response.status_code != 200:
+            raise Exception('Could not create PDF')
+
+        pdf = ContentFile(response.content, name="file.pdf")
+        return HttpResponse(pdf, content_type='application/pdf')
 
 
 class CertificateManualView(EventViewMixin, generic.TemplateView):
