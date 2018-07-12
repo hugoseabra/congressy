6578a1a30c719d6e89f6985fa4248d4c110e8770
Nathan Martins <nathan.eua@gmail.com>
2018-07-12 11:09:14 -0300

    Retirando métodos que não são mais utilizados

--- gatheros_subscription/views/helpers.py
+++ gatheros_subscription/views/helpers.py
@@ -1,87 +1,36 @@
-import csv
+
+class MappingNotFoundError(Exception):
+    pass
+
 
 KEY_MAP = {
     'name': {
         'verbose_name': 'Nome',
         'description': 'Nome do participante',
-        'keys': ['nome'],
+        'csv_keys': ['nome'],
         'possible_values': [],
     },
 
     'email': {
         'verbose_name': 'Email',
         'description': 'Email do participante',
-        'keys': ['email', 'e-mail'],
+        'csv_keys': ['email', 'e-mail'],
         'possible_values': [],
     },
 }
 
-
-def validate_table_keys(line: list) -> dict:
-    main_dict = {}
-
-    for key in line:
-
-        lower_key = key.lower().strip()
-        is_valid = False
-        index = line.index(key)
-
-        for map_key, entry in KEY_MAP.items():
-            allowed_keys = entry['keys']
-            if lower_key in allowed_keys:
-                is_valid = True
-                key = map_key
-                break
-
-        current_dict = {
-            'valid': is_valid,
-            'name': key,
-        }
-
-        main_dict.update({index: current_dict})
-
-    return main_dict
+REQUIRED_KEYS = [
+    'name',
+    'email',
+]
 
 
-def parse_file(encoded_content: str, delimiter: str, quotechar: str):
-    content = encoded_content.splitlines()
+def get_mapping_key_from_csv_key(key):
 
-    first_line = content[0].split(delimiter)
+    for map_key, mapping in KEY_MAP.items():
+        if key in mapping['csv_keys']:
+            return map_key, KEY_MAP[map_key]
 
-    table_keys = validate_table_keys(first_line)
+    raise MappingNotFoundError
 
-    valid_keys = []
 
-    for entry in table_keys.items():
-        if entry[1]['valid']:
-            valid_keys.append(entry[1]['name'])
-
-    main_dict = {}
-    for key in valid_keys:
-        main_dict[key] = []
-
-    reader = csv.DictReader(
-        content,
-        delimiter=delimiter,
-        quotechar=quotechar,
-    )
-
-    for row in reader:
-        for item in row.items():
-
-            possible_key = item[0]
-            if possible_key:
-                possible_key = possible_key.lower().strip()
-
-            possible_value = item[1]
-            if possible_value:
-                possible_value.strip()
-
-            if possible_value and possible_key:
-                for map_key, entry in KEY_MAP.items():
-                    allowed_keys = entry['keys']
-                    if possible_key in allowed_keys:
-                        main_dict[map_key].append(possible_value)
-                        break
-
-    return main_dict
