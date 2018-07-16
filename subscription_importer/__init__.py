afc791cfdf33634e11b371e54fcaca49ea2e801a
Nathan Martins <nathan.eua@gmail.com>
2018-07-16 16:51:21 -0300

    Atualizando __init__

--- subscription_importer/__init__.py
+++ subscription_importer/__init__.py
@@ -1,10 +1,12 @@
 from .exceptions import (
     MappingNotFoundError,
     DataColumnError,
+    NoValidColumnsError,
 )
 from .constants import (
     KEY_MAP,
 )
 from .data_file_transformer import DataFileTransformer
 from .csv_form_integrator import CSVFormIntegrator
+from .column_validator import ColumnValidator
 
