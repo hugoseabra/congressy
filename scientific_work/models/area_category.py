3cc702d1bcd10a1d1b9f7d487508c9c67d7b0eab
Nathan Martins <nathan.eua@gmail.com>
2018-07-16 15:26:19 -0300

    Adicionando metas em models para melhorar o admin

--- scientific_work/models/area_category.py
+++ scientific_work/models/area_category.py
@@ -4,6 +4,10 @@ from gatheros_event.models import Event
 
 class AreaCategory(models.Model):
 
+    class Meta:
+        verbose_name = 'Categoria de Área'
+        verbose_name_plural = 'Categorias de Àreas'
+
     name = models.CharField(
         max_length=25,
         verbose_name="área temática",
