12eb2812133d06874af0d3340fc13b244593d087
Nathan Martins <nathan.eua@gmail.com>
2018-07-16 17:27:21 -0300

    Implementando o LineKeyValidator e tornando o mixin mais 'burro'

--- subscription_importer/line_validators.py
+++ subscription_importer/line_validators.py
@@ -6,15 +6,10 @@ from gatheros_subscription.models import Lot
 class ValidatorMixin(object):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self,
-                 line: dict,
-                 lot_pk: int,
-                 extra_required_keys: dict) -> None:
+    def __init__(self,line: dict) -> None:
         self.errors = {}
         self.valid = False
         self.line = line
-        self.extra_keys = extra_required_keys
-        self.lot = Lot.objects.get(pk=lot_pk)
         super().__init__()
 
     @abc.abstractmethod
@@ -27,6 +22,16 @@ class ValidatorMixin(object):
 
 class LineDataIntegrityValidator(ValidatorMixin):
 
+    def __init__(self,
+                 lot_pk: int,
+                 extra_required_keys: dict = {},
+                 *args, **kwargs) -> None:
+
+        self.extra_keys = extra_required_keys
+        self.lot = Lot.objects.get(pk=lot_pk)
+        super().__init__(*args, **kwargs)
+
+
     def is_valid(self):
         # TODO implement clean logic here
         return self.valid
@@ -34,7 +39,28 @@ class LineDataIntegrityValidator(ValidatorMixin):
 
 class LineKeyValidator(ValidatorMixin):
 
+    def __init__(self, valid_keys: list, *args, **kwargs) -> None:
+        self.valid_keys = valid_keys
+        super().__init__(*args, **kwargs)
+        self.line = self.parse_line_keys()
+
+    def parse_line_keys(self) -> dict:
+        parsed_dict = {}
+
+        for key, value in self.line.items():
+            key = key.lower().strip()
+            parsed_dict.update({key: value})
+
+        return parsed_dict
+
     def is_valid(self):
-        # TODO implement clean logic here
+
+        for key in self.valid_keys:
+            found = self.line.get(key, False)
+
+            if found is False:
+                return False
+
+        self.valid = True
         return self.valid
 
