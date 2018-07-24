4b553af38543f79f847a25642eef7297d45da0c6
Nathan Martins <nathan.eua@gmail.com>
2018-07-24 16:19:43 -0300

    Implementando um loader

--- csv_importer/forms/csv_process_form.py
+++ csv_importer/forms/csv_process_form.py
@@ -10,8 +10,3 @@ class CSVProcessForm(forms.Form):
         widget=forms.HiddenInput,
         required=False,
     )
-
-    create_subscriptions_and_error_xls = forms.BooleanField(
-        widget=forms.HiddenInput,
-        required=False,
-    )
