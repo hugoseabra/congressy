6a29cbd2ad5159a0714285e0ed37f6fb41ef7b69
Hugo Seabra <hugoseabra19@gmail.com>
2018-07-24 21:49:31 -0300

    Correção: SITE_ID de apps.

--- project/admin/settings/__init__.py
+++ project/admin/settings/__init__.py
@@ -30,7 +30,7 @@ ACCOUNT_REGISTRATION = False
 # ========================= SERVER CONFIGURATION ============================ #
 WSGI_APPLICATION = 'project.wsgi.application'
 
-SITE_ID = 2
+SITE_ID = 3
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
