d4252e16953650692a5812df0d6417e8b6da4837
Nathan Martins <nathan.eua@gmail.com>
2018-08-13 14:24:42 -0300

    Feature Configuration: Teste e implementação do helper para testar se evento é gŕatis ou não

---
+++ gatheros_subscription/helpers/feature_identifier.py
@@ -0,0 +1,36 @@
+"""
+    Esses helpers tem como objetivo identificar o tipo do evento:
+
+        - Grátis
+        - Pago
+        - Cientifico
+"""
+from gatheros_event.models import Event
+
+
+def is_free_event(event: Event):
+
+    lots = event.lots.all()
+
+    for lot in lots:
+
+        if lot.price and lot.price > 0:
+            return False
+
+        if hasattr(lot, 'category') and lot.category is not None:
+
+            category = lot.category
+            services = category.service_optionals
+            products = category.product_optionals
+
+            if services is not None:
+                for service in services.all():
+                    if service.liquid_price > 0:
+                        return False
+
+            if products is not None:
+                for product in products.all():
+                    if product.liquid_price > 0:
+                        return False
+
+    return True
--- gatheros_subscription/tests/domain/test_feature_configuration_helpers.py
+++ gatheros_subscription/tests/domain/test_feature_configuration_helpers.py
@@ -1,31 +1,58 @@
-from datetime import datetime, timedelta
-
 from django.test import TestCase
 
 from gatheros_event.tests.mocks import MockFactory as EventMockFactory
+from addon.tests import MockFactory as AddonMockFactory
 from gatheros_subscription.helpers.feature_identifier import (
     is_free_event,
-    is_paid_event,
 )
-from gatheros_subscription.models import Lot
+from gatheros_subscription.tests.mocks import MockFactory
 
 
 class FeatureConfigurationTest(TestCase):
 
     def setUp(self):
-        self.fake_factory = EventMockFactory()
+        self.event_mock_factory = EventMockFactory()
+        self.addon_mock_factory = AddonMockFactory()
+        self.mock_factory = MockFactory()
 
     def test_is_free(self):
-        event = self.fake_factory.fake_event()
-        self.assertTrue(is_free_event(event))
+        # Testing with a truly free event
+        clean_event = self.event_mock_factory.fake_event()
+        self.assertTrue(is_free_event(clean_event))
 
-    def test_is_paid(self):
-        event = self.fake_factory.fake_event()
-        Lot.objects.create(
-            event=event,
-            name=self.fake_factory.fake_factory.name(),
-            date_start=datetime.now() + timedelta(days=1),
-            price=1,
+        # Testing with a paid by lote event.
+        paid_lot_event = self.event_mock_factory.fake_event()
+        self.mock_factory.fake_paid_lot(event=paid_lot_event)
+        self.assertFalse(is_free_event(paid_lot_event))
+
+        # Test with paid optional service
+        paid_opt_service_event = self.event_mock_factory.fake_event()
+
+        paid_opt_service_lot = self.mock_factory.fake_lot(
+            paid_opt_service_event)
+        paid_opt_service_lot_cat = self.mock_factory.fake_lot_category(
+            paid_opt_service_event)
+        self.mock_factory.add_category_to_lot(paid_opt_service_lot,
+                                              paid_opt_service_lot_cat)
+
+        self.addon_mock_factory.fake_service(
+            lot_category=paid_opt_service_lot_cat,
+            price=10,
         )
+        self.assertFalse(is_free_event(paid_opt_service_event))
 
-        self.assertTrue(is_paid_event(event))
+        # Test with paid optional product
+        paid_opt_product_event = self.event_mock_factory.fake_event()
+
+        paid_opt_product_lot = self.mock_factory.fake_lot(
+            paid_opt_product_event)
+        paid_opt_product_lot_cat = self.mock_factory.fake_lot_category(
+            paid_opt_product_event)
+        self.mock_factory.add_category_to_lot(paid_opt_product_lot,
+                                              paid_opt_product_lot_cat)
+
+        self.addon_mock_factory.fake_product(
+            lot_category=paid_opt_product_lot_cat,
+            price=10,
+        )
+        self.assertFalse(is_free_event(paid_opt_product_event))
