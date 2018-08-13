6ad927c4499e8b3414d763b9729298d0b44e4077
Nathan Martins <nathan.eua@gmail.com>
2018-08-13 14:24:08 -0300

    Feature Configuration: Criação de um Mock Factory para o app de Subscriptions

---
+++ gatheros_subscription/tests/mocks.py
@@ -0,0 +1,66 @@
+"""
+    Mock factory used during tests to create required gatheros subscription
+    domain objects.
+"""
+from datetime import datetime, timedelta
+
+from faker import Faker
+
+from gatheros_event.models import Event
+from gatheros_subscription.models import Lot, LotCategory
+
+
+class MockFactory:
+    """
+        Mock Factory Implementation
+    """
+
+    def __init__(self):
+        self.fake_factory = Faker('pt_BR')
+
+    def fake_lot(self, event: Event):
+        if not isinstance(event, Event):
+            raise Exception("'{}' is not instance of Event".format(event))
+
+        return Lot.objects.create(
+            event=event,
+            name=self.fake_factory.name(),
+            date_start=datetime.now() + timedelta(days=2)
+        )
+
+    def fake_paid_lot(self, event: Event, amount: int = 10):
+        if not isinstance(event, Event):
+            raise Exception("'{}' is not instance of Event".format(event))
+
+        return Lot.objects.create(
+            event=event,
+            price=amount,
+            name=self.fake_factory.name(),
+            date_start=datetime.now() + timedelta(days=2)
+        )
+
+    def fake_lot_category(self, event: Event):
+        if not isinstance(event, Event):
+            raise Exception("'{}' is not instance of Event".format(event))
+
+        return LotCategory.objects.create(
+            event=event,
+            name=self.fake_factory.name(),
+            active=True,
+        )
+
+    @staticmethod
+    def add_category_to_lot(lot: Lot, category: LotCategory):
+        if not isinstance(lot, Lot):
+            raise Exception("'{}' is not instance of Lot".format(lot))
+
+        if not isinstance(category, LotCategory):
+            raise Exception(
+                "'{}.{}' is not instance of LotCategory".format(
+                    category,
+                    category.__class__,
+                )
+            )
+
+        lot.category = category
+        lot.save()
