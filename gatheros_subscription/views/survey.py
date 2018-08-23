41471e57782d672161b2cc08c062bd18e3f4e63f
Hugo Seabra <hugoseabra19@gmail.com>
2018-08-23 16:51:55 -0300

    Correção: Survey: ordenação de campos corrigida - #381

--- gatheros_subscription/views/survey.py
+++ gatheros_subscription/views/survey.py
@@ -72,14 +72,13 @@ class SurveyEditView(EventViewMixin, AccountMixin, generic.TemplateView,
 
             elif action == 'update_order':
 
-                new_order = json.loads(self.request.POST.get(
-                    'new_order'))
+                new_order = json.loads(self.request.POST.get('new_order'))
 
                 counter = 1
                 try:
-                    for question_name in new_order:
+                    for question_id in new_order:
                         question = Question.objects.get(survey=self.survey,
-                                                        name=question_name)
+                                                        pk=question_id)
                         question.order = counter
                         question.save()
                         counter += 1
