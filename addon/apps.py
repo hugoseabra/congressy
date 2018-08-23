1e8aec736a4695382e678bbe626bb34aeaa8a173
Hugo Seabra <hugoseabra19@gmail.com>
2018-08-23 19:31:14 -0300

    Melhoria - Addon: apagar arquivos anteriores ao subir uma nova - #388

--- addon/apps.py
+++ addon/apps.py
@@ -5,3 +5,7 @@ from django.apps import AppConfig
 class AddonConfig(AppConfig):
     name = 'addon'
     verbose_name = 'Opcionais'
+
+    # noinspection PyUnresolvedReferences
+    def ready(self):
+        import addon.signals
