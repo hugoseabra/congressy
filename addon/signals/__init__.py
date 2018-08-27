2cea92d6f1528ae5110107c4a2c8f11e50008609
Nathan Martins <nathan.eua@gmail.com>
2018-08-27 13:14:27 -0300

    Merge conflitcs

--- addon/signals/__init__.py
+++ addon/signals/__init__.py
@@ -1,3 +1,2 @@
-from . import optional_signals
 """ signals de addons """
 from . import addon_signals
--- addon/signals/addon_signals.py
+++ addon/signals/addon_signals.py
@@ -1,5 +1,8 @@
 """ Signals do model `addons` """
-from django.db.models.signals import post_save, pre_delete
+import os
+import shutil
+
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 
 from addon.models import Service, Product
@@ -15,3 +18,36 @@ def set_feature_flags_on_event_type_change(instance, **_):
 
     event = instance.lot_category.event
     update_event_config_flags(event)
+
+
+@receiver(post_save, sender=Product)
+@receiver(post_save, sender=Service)
+def erase_previous_files(instance, raw, created, **_):
+    if raw is True and created is True:
+        return
+
+    if instance.has_changed('banner'):
+        current_banner_path = instance.banner.path
+
+        # Inclui os arquivos do StdImage
+        current_paths = [
+            current_banner_path,
+            instance.banner.default.path,
+            instance.banner.thumbnail.path,
+        ]
+
+        base_dir = os.path.dirname(current_banner_path)
+        if not os.path.isdir(base_dir):
+            return
+
+        for file in os.listdir(base_dir):
+            file_path = os.path.join(base_dir, file)
+
+            if file_path in current_paths:
+                continue
+
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
