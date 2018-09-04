24cdc1925c460ec44fbcef6b62551a3e6b6b6f4d
Nathan Martins <nathan.eua@gmail.com>
2018-09-04 13:09:21 -0300

    Draft: Criar testes para a spec 'is_publishable' - Funcionalidade #466

--- gatheros_event/tests/domain/specifications/test_publishable_specification.py
+++ gatheros_event/tests/domain/specifications/test_publishable_specification.py
@@ -12,33 +12,37 @@ class EventPublishableSpecificationTest(TestCase):
         event.lots.all().delete()
         return event
 
+    @staticmethod
+    def _set_description(event):
+        event.info.description = 'aaaa'
+        event.save()
+
     def setUp(self):
         self.event_factory = EventFactory()
-
-        self.event_factory = EventFactory()
         self.sub_factory = SubFactory()
+        self.spec = EventPublishable()
 
-        self.free_event = self.event_factory.fake_event()
-        self.paid_event = self.event_factory.fake_event()
+    def test_unsubscribable_event(self):
+        event = self._clean_event(self.event_factory.fake_event())
+        self._set_description(event)
+        self.assertFalse(self.spec.is_satisfied_by(event))
+        self.sub_factory.fake_lot(event=event)
+        self.assertTrue(self.spec.is_satisfied_by(event))
+
+    def test_undescribed_event(self):
+        event = self.event_factory.fake_event()
+        self.sub_factory.fake_lot(event=event)
+        self.assertFalse(self.spec.is_satisfied_by(event))
+        self._set_description(event)
+        self.assertTrue(self.spec.is_satisfied_by(event))
+
+    def test_paid_event(self):
+        event = self.event_factory.fake_event()
+        self._set_description(event)
         for i in range(4):
-            self.sub_factory.fake_paid_lot(event=self.paid_event)
-
-    # ======= is_specification =======
-
-    def test_paid_event_is_spec(self):
-        root_specification = EventPublishable()
-        self.assertTrue(root_specification.is_satisfied_by(self.paid_event))
-
-    def test_free_event_is_spec(self):
-        root_specification = EventPublishable()
-        self.assertTrue(root_specification.is_satisfied_by(self.free_event))
-
-    # ======= not_specification =======
-
-    def test_paid_event_not_spec(self):
-        root_specification = EventPublishable().not_specification()
-        self.assertFalse(root_specification.is_satisfied_by(self.paid_event))
-
-    def test_free_event_not_spec(self):
-        root_specification = EventPublishable().not_specification()
-        self.assertFalse(root_specification.is_satisfied_by(self.free_event))
+            self.sub_factory.fake_paid_lot(event=event)
+        # self.assertFalse(self.spec.is_satisfied_by(event))
+        paying_org = self.event_factory.fake_paying_organization()
+        event.organization = paying_org
+        event.save()
+        self.assertTrue(self.spec.is_satisfied_by(event))
--- gatheros_event/tests/mocks.py
+++ gatheros_event/tests/mocks.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 from faker import Faker
 
-from gatheros_event.models import Person, Event, Organization, Category, Member
+from gatheros_event.models import Person, Event, Organization, Category, Member, Info
 
 
 class MockFactory:
@@ -38,15 +38,20 @@ class MockFactory:
         return person
 
     def fake_organization(self):
-        organization = Organization(
+        return Organization.objects.create(
             name=self.fake_factory.company(),
             email=self.fake_factory.ascii_email(),
         )
-        organization.save()
 
-        assert organization is not None
-
-        return organization
+    def fake_paying_organization(self):
+        org = self.fake_organization()
+        org.bank_code = Organization.BANCO_DO_BRASIL
+        org.agency = '001'
+        org.account = '001'
+        org.cnpj_ou_cpf = '36876747034'
+        org.account_type = Organization.CONTA_CORRENTE
+        org.save()
+        return org
 
     def fake_event(self, organization=None, date_start=None, date_end=None):
 
@@ -59,13 +64,19 @@ class MockFactory:
         if not organization:
             organization = self.fake_organization()
 
-        return Event.objects.create(
+        event = Event.objects.create(
             name='Event: ' + ' '.join(self.fake_factory.words(nb=3)),
             organization=organization,
             date_start=date_start,
             date_end=date_end,
             category=Category.objects.first(),
         )
+
+        Info.objects.create(
+            event=event
+        )
+
+        return event
 
     @staticmethod
     def join_organization(organization, person):
