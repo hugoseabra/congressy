35bd45e3d5d5da075d6c8d4d022fa64775457358
Hugo Seabra <hugoseabra19@gmail.com>
2018-09-19 19:47:32 -0300

    Correção simples de processamento de débitos devido a virada de versão.

--- payment_debt/management/commands/update_payments.py
+++ payment_debt/management/commands/update_payments.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         subs_notransctions = []
 
         for event in Event.objects.all():
-            if is_paid_event(event) is False:
+            if EventPayable().is_satisfied_by(event) is False:
                 continue
 
             for sub in event.subscriptions.all():
