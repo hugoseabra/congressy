9706a08ccb6932f53db2db405b9c3a4285939932
Hugo Seabra <hugoseabra19@gmail.com>
2018-09-19 15:45:26 -0300

    Ajustes de deploy.

--- conf/staging/services/migration/container-entry.sh
+++ conf/staging/services/migration/container-entry.sh
@@ -9,6 +9,10 @@ run_python_script "Configurando SETTINGS" /configure-settings.py
 
 run_python_script_with_output "Executando migrate" "manage.py migrate"
 
+
+# ADICIONA SUPER USUÁRIOS ÀS ORGANIZAÇÕES COM EVENTOS
+run_python_script_with_output "Adicionando admins à organizações" "manage.py admins_to_organizations"
+
 # TEMP
 run_python_script_with_output "Migrando infraestrutura de debitos" "manage.py update_debts"
 run_python_script_with_output "Migrando infraestrutura de pagamento" "manage.py update_payments"
