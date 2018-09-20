fa10fe177c43b990677dd8afb57b8731c4def326
Nathan Martins <nathan.eua@gmail.com>
2018-09-20 09:25:30 -0300

    .

--- gatheros_subscription/views/lotcategory.py
+++ gatheros_subscription/views/lotcategory.py
@@ -20,6 +20,7 @@ except ImportError:
     SENTRY_RAVEN = False
 
 
+
 class LotCategoryListView(AccountMixin, generic.ListView, EventDraftStateMixin):
     """Lista de lotes de acordo com o evento do contexto"""
     model = LotCategory
