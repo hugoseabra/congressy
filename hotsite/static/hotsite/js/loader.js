18748a7eb7d570a1e3bbad9d163d384aae15bb47
Hugo Seabra <hugoseabra19@gmail.com>
2018-09-29 10:42:29 -0300

    Ajustando botões de formulário do hotsite.

--- hotsite/static/hotsite/js/loader.js
+++ hotsite/static/hotsite/js/loader.js
@@ -1,31 +1,11 @@
-function next_page(form_id) {
-    
-    var form = document.getElementById(form_id);
+function next_page() {
 
     var wizard_next_btn_el = $('.wizard_next_btn');
     var wizard_next_loader_el = $('.wizard_next_loader');
 
-    if (form_is_valid(form)) {
-        wizard_next_btn_el.hide();
-        wizard_next_loader_el.show();
-        $('#' + form_id).submit()
-    } else {
-        alert('Existem alguns campos obrigatórios não preenchidos!');
-    }
+    wizard_next_btn_el.hide();
+    wizard_next_loader_el.show();
 
-
-}
-
-
-function form_is_valid(form_el) {
-
-    for (var i = 0; i < form_el.elements.length; i++) {
-        if (form_el.elements[i].value === '' && form_el.elements[i].hasAttribute('required')) {
-            return false;
-        }
-    }
 
     return true;
-
 }
-
