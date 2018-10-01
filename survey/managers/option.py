d1ff5f352109625e17a192ae8d8f6ba39d1525ea
Nathan Martins <nathan.eua@gmail.com>
2018-10-01 12:01:11 -0300

    Erro no Form de Survey do hotsite - Funcionalidade #757

--- survey/managers/option.py
+++ survey/managers/option.py
@@ -37,10 +37,9 @@ class OptionManager(Manager):
 
         return question
 
-    def clean(self):
-        cleaned_data = super().clean()
-        cleaned_data['value'] = self._create_slug()
-        return cleaned_data
+    def save(self, commit=True):
+        self.instance.value = self._create_slug()
+        return super().save(commit)
 
     def _create_slug(self):
         """
