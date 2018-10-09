c3424c5ea0b66e92146a6c32b8eec428ba017f1c
Hugo Seabra <hugoseabra19@gmail.com>
2018-10-09 17:45:23 -0300

    mix_boleto: view de hook de gatilho de sincronização de todas as inscrições.

--- mix_boleto/mix/__init__.py
+++ mix_boleto/mix/__init__.py
@@ -3,3 +3,5 @@ from .category import MixCategory
 from .lot import MixLot
 from .subscription import MixSubscription
 from .boleto import MixBoleto
+from .subscription_collection_builder import MixSubscriptionCollectionBuilder
+from .sync import MixSync
