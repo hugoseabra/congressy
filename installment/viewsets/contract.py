1946f3fe1eeca67b26e5dc1a48a190bd887b5828
Nathan Martins <nathan.eua@gmail.com>
2018-11-20 08:54:39 -0200

    Limpando methodos redundantes na viewset

--- installment/viewsets/contract.py
+++ installment/viewsets/contract.py
@@ -1,6 +1,3 @@
-from rest_framework import status
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from installment.models import Contract
@@ -28,39 +25,3 @@ class ContractViewSet(RestrictionViewMixin, ModelViewSet):
             return qs.filter(subscription__event_id=event_pk)
 
         return qs
-
-    def list(self, request, *args, **kwargs):
-
-        qs = self.get_queryset()
-
-        page = self.paginate_queryset(qs)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-
-        queryset = self.get_queryset()
-
-        # Perform the lookup filtering.
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-
-        assert lookup_url_kwarg in self.kwargs, (
-                'Expected view %s to be called with a URL keyword argument '
-                'named "%s". Fix your URL conf, or set the `.lookup_field` '
-                'attribute on the view correctly.' %
-                (self.__class__.__name__, lookup_url_kwarg)
-        )
-
-        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-        obj = get_object_or_404(queryset, **filter_kwargs)
-
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-
-        instance = obj
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
