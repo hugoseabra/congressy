5eaf6c8ced84479d635440a16b33aed55611c766
Nathan Martins <nathan.eua@gmail.com>
2018-11-23 13:32:52 -0200

    Pequenas correções

--- installment/serializers/part.py
+++ installment/serializers/part.py
@@ -7,6 +7,11 @@ from installment.services import PartService
 
 class PartSerializer(FormSerializerMixin,
                      serializers.ModelSerializer):
+    
+    expiration_date = serializers.DateField(input_formats=[
+        '%d/%m/%Y',
+    ])
+
     class Meta:
         form = PartService
         model = Part
