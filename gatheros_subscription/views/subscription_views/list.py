d8d18cb2fb74dd43fc294896dfa77c7ef974e788
Nathan Martins <nathan.eua@gmail.com>
2019-03-25 16:27:45 -0300

    Surpote parcial para filtro de instituição

--- gatheros_subscription/views/subscription_views/list.py
+++ gatheros_subscription/views/subscription_views/list.py
@@ -1,9 +1,10 @@
+import locale
 from django.views import generic
+from functools import cmp_to_key
 
 from gatheros_event.helpers.event_business import is_paid_event
 from gatheros_subscription.models import (
-    Subscription,
-)
+    Subscription)
 from gatheros_subscription.views import SubscriptionViewMixin
 
 
@@ -12,19 +13,18 @@ class SubscriptionListView(SubscriptionViewMixin, generic.TemplateView):
 
     model = Subscription
     template_name = 'subscription/list.html'
-    has_filter = False
 
     def get_context_data(self, **kwargs):
         cxt = super().get_context_data(**kwargs)
 
         cxt.update({
-            'lots': None,
             'has_subs': Subscription.objects.filter(
                 event_id=self.event.id,
                 completed=True,
                 test_subscription=False,
             ).count() > 0,
-            'has_filter': self.has_filter,
+            'institutions': self.get_institutions(),
+            'lots': self.get_lots(),
             'event_is_paid': is_paid_event(self.event),
             'has_inside_bar': True,
             'active': 'inscricoes',
@@ -36,3 +36,51 @@ class SubscriptionListView(SubscriptionViewMixin, generic.TemplateView):
             'gatheros_event.can_manage_subscriptions',
             self.get_event()
         )
+
+    def get_institutions(self):
+        institutions = list()
+
+        for sub in self.event.subscriptions.all():
+
+            if sub.person.institution:
+
+                institution = str.strip(sub.person.institution)
+                if institution not in institutions:
+                    institutions.append(institution)
+
+        return sorted(institutions, key=cmp_to_key(locale.strcoll))
+    #
+    # def get_categories(self):
+    #
+    #     categories = dict()
+    #
+    #     for cat in LotCategory.objects.filter(event=self.event):
+    #         if cat.pk not in categories.values():
+    #             categories[cat.name] = cat.pk
+    #
+    #     sorted_categories = OrderedDict()
+    #
+    #     for name in sorted(categories.keys(), key=cmp_to_key(locale.strcoll)):
+    #         for _name, pk in categories.items():
+    #             if name == _name:
+    #                 sorted_categories[name] = pk
+    #
+    #     return sorted_categories
+    #
+    # def get_lots(self):
+    #
+    #     lots = dict()
+    #
+    #     for lot in Lot.objects.filter(event=self.event, internal=False):
+    #
+    #         if lot.pk not in lots.values():
+    #             lots[lot.name] = lot.pk
+    #
+    #     sorted_lots = OrderedDict()
+    #
+    #     for name in sorted(lots.keys(), key=cmp_to_key(locale.strcoll)):
+    #         for _name, pk in lots.items():
+    #             if name == _name:
+    #                 sorted_lots[name] = pk
+    #
+    #     return sorted_lots
