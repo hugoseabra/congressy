6ebb673d46fa4289b07f964c93320bd3ae7eb2f5
Hugo Seabra <hugoseabra19@gmail.com>
2019-03-27 12:14:56 -0300

    Suporte a UNACCENT do postgresql como patch de busca do queryset

---
+++ core/database/postgresql.py
@@ -0,0 +1,20 @@
+from django.db.backends.postgresql_psycopg2.base import (
+    DatabaseOperations,
+    DatabaseWrapper,
+)
+
+
+def lookup_cast(self, lookup_type, internal_type=None):
+    if lookup_type in ('icontains', 'istartswith'):
+        return "UPPER(unaccent(%s::text))"
+    else:
+        return super(DatabaseOperations, self).lookup_cast(lookup_type,
+                                                           internal_type)
+
+
+def patch_unaccent():
+    DatabaseOperations.lookup_cast = lookup_cast
+    DatabaseWrapper.operators['icontains'] = 'LIKE UPPER(unaccent(%s))'
+    DatabaseWrapper.operators['istartswith'] = 'LIKE UPPER(unaccent(%s))'
+
+    print('Unaccent patch applied')
