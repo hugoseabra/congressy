648d2be90c9c8e052966a3fdf10e12e4371773c5
Nathan Martins <nathan.eua@gmail.com>
2019-03-29 17:39:45 -0300

    Adiconando aba de detalhamento

--- attendance/views/export.py
+++ attendance/views/export.py
@@ -5,9 +5,10 @@ from django.views.generic import View
 from openpyxl import Workbook
 from six import BytesIO
 
-from attendance.models import AttendanceService, Checkin
+from attendance.models import AttendanceService, Checkin, Checkout
 from gatheros_subscription.helpers.export import clean_sheet_title, \
     get_object_value
+from gatheros_subscription.models import Subscription
 from .mixins import AttendancesFeatureFlagMixin
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
@@ -52,7 +53,6 @@ def export_attendance(attendance_service_pk=None):
 
     ws1 = wb.active
     ws1.title = clean_sheet_title('Participantes')
-    # wb.create_sheet(title='Detalhamento')
 
     if attendance_service_pk is None:
         ws1.append(["ERRO: Identificador de serviço não informado."])
@@ -81,6 +81,17 @@ def export_attendance(attendance_service_pk=None):
         ws1.append(["Nenhuma inscrição neste serviço de atendimento"])
     else:
         export_attendance_checkin_subscriptions(ws1, subscriptions, service)
+
+    ws2 = wb.create_sheet(title='Detalhamento')
+
+    subscriptions = list()
+    for sub in Subscription.objects.filter(event__attendance_services=service):
+        subscriptions.append(sub)
+
+    if len(subscriptions) == 0:
+        ws2.append(["Nenhuma inscrição neste serviço de atendimento"])
+    else:
+        export_attendance_detailed_subscriptions(ws2, subscriptions, service)
 
     wb.save(stream)
     return stream.getvalue()
@@ -236,6 +247,190 @@ def export_attendance_checkin_subscriptions(worksheet, subscriptions,
                 checkin.registration.strftime('%d/%m/%Y %H:%M:%S'))
         else:
             '----'
+
+        row_idx += 1
+
+    for row in collector.keys():
+        worksheet.append(collector[row])
+
+
+def export_attendance_detailed_subscriptions(worksheet, subscriptions,
+                                             attendance):
+    """ Exporta dados de inscrição. """
+
+    worksheet.append([
+        'NÚMERO DE INSCRIÇÃO',
+        'CÓDIGO DA INSCRIÇÃO',
+        'CATEGORIA DE PARTICIPANTE',
+        'LOTE',
+        'STATUS',
+        'SEXO',
+        'NOME',
+        'TIPO DE DOCUMENTO',
+        'NÚMERO DO DOCUMENTO',
+        'DATA NASC',
+        'IDADE',
+        'EMAIL',
+        'PHONE',
+        'RUA/LOGRADOURO',
+        'COMPLEMENTO',
+        'NUMERO',
+        'BAIRRO',
+        'CEP',
+        'CIDADE',
+        'UF',
+        'PAÍS',
+        'INSTITUICAO/EMPRESA',
+        'CNPJ',
+        'FUNÇÃO/CARGO',
+        'TAG DE AGRUPAMENTO',
+        'INFO. PARA CRACHÁ',
+        'OBS',
+        'CRIADO EM',
+        'CHECKINS',
+        'CHECKOUTS',
+    ])
+
+    collector = {}
+    row_idx = 1
+    for sub in subscriptions:
+
+        checkins = Checkin.objects.filter(
+            subscription=sub,
+            attendance_service=attendance,
+        )
+
+        checkins = [c for c in checkins]
+
+        checkin_pks = [c.pk for c in checkins]
+
+        checkouts = Checkout.objects.filter(
+            checkin__in=checkin_pks
+        )
+
+        checkouts = [c for c in checkouts]
+
+        if row_idx not in collector:
+            collector[row_idx] = []
+
+        person = get_object_value(
+            obj=sub,
+            attr='person',
+            cached_type='subscriptions',
+        )
+
+        lot = get_object_value(
+            obj=sub,
+            attr='lot',
+            cached_type='subscriptions',
+        )
+
+        lot_category = get_object_value(
+            obj=lot,
+            attr='category',
+            cached_type='lots',
+        )
+
+        city = get_object_value(
+            obj=person,
+            attr='city',
+            cached_type='people',
+        )
+
+        collector[row_idx].append(get_object_value(
+            obj=sub,
+            attr='event_count',
+            cached_type='subscriptions',
+        ))
+
+        collector[row_idx].append(get_object_value(
+            obj=sub,
+            attr='code',
+            cached_type='subscriptions',
+        ))
+
+        collector[row_idx].append(lot_category.name)
+
+        collector[row_idx].append(lot.name)
+
+        collector[row_idx].append(sub.get_status_display())
+        collector[row_idx].append(person.get_gender_display())
+
+        collector[row_idx].append(get_object_value(person, 'name'))
+
+        country = get_object_value(person, 'country')
+        if country == 'BR':
+            doc_type = 'CPF'
+            doc_num = get_object_value(person, 'cpf')
+        else:
+            doc_type = get_object_value(person, 'international_doc_type')
+            doc_num = get_object_value(person, 'international_doc')
+
+        collector[row_idx].append(doc_type)
+        collector[row_idx].append(doc_num)
+
+        if person.birth_date:
+            collector[row_idx].append(person.birth_date.strftime('%d/%m/%Y'))
+            collector[row_idx].append(person.age)
+        else:
+            collector[row_idx].append('')
+            collector[row_idx].append('')
+
+        collector[row_idx].append(get_object_value(person, 'email'))
+        collector[row_idx].append(get_object_value(person, 'phone'))
+        if country == 'BR':
+            street = get_object_value(person, 'street')
+            number = get_object_value(person, 'number')
+            village = get_object_value(person, 'village')
+            zip_code = get_object_value(person, 'zip_code')
+            city_name = get_object_value(city, 'name')
+            uf = get_object_value(city, 'uf')
+
+        else:
+            street = get_object_value(person, 'address_international')
+            number = ''
+            village = ''
+            zip_code = get_object_value(person, 'zip_code_international')
+            city_name = get_object_value(person, 'city_international')
+            uf = get_object_value(person, 'state_international')
+
+        collector[row_idx].append(street)
+        collector[row_idx].append(get_object_value(person, 'complement'))
+        collector[row_idx].append(number)
+        collector[row_idx].append(village)
+        collector[row_idx].append(zip_code)
+        collector[row_idx].append(city_name)
+        collector[row_idx].append(uf)
+        collector[row_idx].append(country)
+        collector[row_idx].append(get_object_value(person, 'institution'))
+        collector[row_idx].append(get_object_value(person, 'institution_cnpj'))
+        collector[row_idx].append(get_object_value(person, 'function'))
+        collector[row_idx].append(get_object_value(sub, 'tag_group'))
+        collector[row_idx].append(get_object_value(sub, 'tag_info'))
+        collector[row_idx].append(get_object_value(sub, 'obs'))
+        collector[row_idx].append(sub.created.strftime('%d/%m/%Y %H:%M:%S'))
+
+        checkin_str = ''
+        for i in range(1, len(checkins)):
+            checkin_str += "Checkin {}º feito ás {} \n".format(
+                i,
+                checkins[i - 1].created_on.strftime('%H:%M:%S de %d/%m/%Y '))
+
+        if checkin_str == '':
+            checkin_str = '----'
+
+        collector[row_idx].append(checkin_str)
+
+        checkout_str = ''
+        for i in range(1, len(checkouts)):
+            checkout_str += "Checkout {}º feito ás {} \n".format(
+                i,
+                checkouts[i - 1].created_on.strftime('%H:%M:%S de %d/%m/%Y '))
+
+        if checkout_str == '':
+            checkout_str = '----'
+
+        collector[row_idx].append(checkout_str)
 
         row_idx += 1
 
