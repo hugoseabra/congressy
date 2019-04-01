9dbeec0884426333998765dfa4ef91eaf90d0325
Nathan Martins <nathan.eua@gmail.com>
2019-04-01 17:17:50 -0300

    Correção do bug: Exportação

--- attendance/views/export.py
+++ attendance/views/export.py
@@ -1,5 +1,6 @@
 import locale
 from datetime import datetime
+from django.db.models import QuerySet
 from django.http import HttpResponse
 from django.views.generic import View
 from openpyxl import Workbook
@@ -44,15 +45,16 @@ class AttendanceXLSExportView(AttendancesFeatureFlagMixin, View):
 def export_attendance(attendance_service_pk=None):
     """
     Exportação de inscrições de um serviço de atendimento em formato xlsx
-
     :return: ByteIO
     """
     stream = BytesIO()
 
     wb = Workbook()
 
+    # ===================== PRESENTES ==========================================
+
     ws1 = wb.active
-    ws1.title = clean_sheet_title('Participantes')
+    ws1.title = clean_sheet_title('Presentes')
 
     if attendance_service_pk is None:
         ws1.append(["ERRO: Identificador de serviço não informado."])
@@ -75,30 +77,78 @@ def export_attendance(attendance_service_pk=None):
     subscriptions = list()
 
     for checkin in checkins:
-        subscriptions.append(checkin.subscription)
+        if checkin.subscription not in subscriptions:
+            subscriptions.append(checkin.subscription)
+
+    subscriptions = sort_subscriptions(subscriptions)
 
     if len(subscriptions) == 0:
-        ws1.append(["Nenhuma inscrição neste serviço de atendimento"])
+        ws1.append(["Nenhuma inscrição com presença"])
     else:
-        export_attendance_checkin_subscriptions(ws1, subscriptions, service)
+        export_attendance_checkins(
+            worksheet=ws1,
+            subscriptions=subscriptions,
+            attendance=service
+        )
 
-    ws2 = wb.create_sheet(title='Detalhamento')
+    # ===================== AUSENTES ===========================================
+
+    ws2 = wb.create_sheet(title='Ausentes(Participantes que fizeram checkout)')
+
+    checkouts = Checkin.objects.filter(
+        attendance_service=service,
+        checkout__isnull=False,
+    ).exclude(
+        subscription_id__in=[str(s.pk) for s in subscriptions]
+    ).order_by('registration', 'created_by')
 
     subscriptions = list()
-    for sub in Subscription.objects.filter(event__attendance_services=service):
-        subscriptions.append(sub)
+
+    for checkin in checkouts:
+        if checkin.subscription not in subscriptions:
+            subscriptions.append(checkin.subscription)
+
+    subscriptions = sort_subscriptions(subscriptions)
 
     if len(subscriptions) == 0:
-        ws2.append(["Nenhuma inscrição neste serviço de atendimento"])
+        ws2.append(["Nenhuma inscrição com ausencia"])
     else:
-        export_attendance_detailed_subscriptions(ws2, subscriptions, service)
+        export_attendance_checkouts(
+            worksheet=ws2,
+            subscriptions=subscriptions,
+            attendance=service,
+        )
+
+    # ===================== AUDITORIA ==========================================
+
+    ws3 = wb.create_sheet(title='Auditoria')
+
+    checkins = Checkin.objects.filter(
+        attendance_service=service,
+    ).order_by('registration', 'created_by')
+
+    subscriptions = list()
+
+    for checkin in checkins:
+        if checkin.subscription not in subscriptions:
+            subscriptions.append(checkin.subscription)
+
+    subscriptions = sort_subscriptions(subscriptions)
+
+    if len(subscriptions) == 0:
+        ws3.append(["Nenhuma inscrição neste serviço de atendimento"])
+    else:
+        export_attendance_auditing(
+            worksheet=ws3,
+            subscriptions=subscriptions,
+            attendance=service,
+        )
 
     wb.save(stream)
     return stream.getvalue()
 
 
-def export_attendance_checkin_subscriptions(worksheet, subscriptions,
-                                            attendance):
+def export_attendance_checkins(worksheet, subscriptions, attendance):
     """ Exporta dados de inscrição. """
 
     worksheet.append([
@@ -243,22 +293,37 @@ def export_attendance_checkin_subscriptions(worksheet, subscriptions,
         collector[row_idx].append(get_object_value(sub, 'obs'))
         collector[row_idx].append(sub.created.strftime('%d/%m/%Y %H:%M:%S'))
 
-        if checkins.count() > 0:
+        if checkins.count() > 1:
 
             checkin_str = ''
+
             for i in range(1, len(checkins)):
 
                 instance = checkins[i - 1]
 
                 if instance.registration:
-                    t = instance.registration.strftime('%H:%M:%S de %d/%m/%Y')
+                    t = instance.registration.strftime('%d/%m/%Y %H:%M:%S')
                 else:
-                    t = instance.created_on.strftime('%H:%M:%S de %d/%m/%Y')
+                    t = instance.created_on.strftime('%d/%m/%Y %H:%M:%S')
 
-                checkin_str += "Checkin {}º feito ás {} \n".format(i, t)
+                checkin_str += "{}º ás {} \n".format(i, t)
 
+            # Removing last '\n'
+            checkin_str = checkin_str[:-1]
+
+        elif checkins.count() == 1:
+
+            instance = checkins[0]
+
+            if instance.registration:
+                t = instance.registration.strftime('%d/%m/%Y %H:%M:%S')
+            else:
+                t = instance.created_on.strftime('%d/%m/%Y %H:%M:%S')
+
+            checkin_str = "{}".format(t)
         else:
-            checkin_str = '----'
+            raise Exception("Trying to export checkedin subscription without "
+                            "checkin: {}".format(str(sub.pk)))
 
         collector[row_idx].append(checkin_str)
 
@@ -268,8 +333,7 @@ def export_attendance_checkin_subscriptions(worksheet, subscriptions,
         worksheet.append(collector[row])
 
 
-def export_attendance_detailed_subscriptions(worksheet, subscriptions,
-                                             attendance):
+def export_attendance_checkouts(worksheet, subscriptions, attendance):
     """ Exporta dados de inscrição. """
 
     worksheet.append([
@@ -301,28 +365,17 @@ def export_attendance_detailed_subscriptions(worksheet, subscriptions,
         'INFO. PARA CRACHÁ',
         'OBS',
         'CRIADO EM',
-        'CHECKINS',
-        'CHECKOUTS',
+        'HORA REGISTRADA',
     ])
 
     collector = {}
     row_idx = 1
     for sub in subscriptions:
 
-        checkins = Checkin.objects.filter(
-            subscription=sub,
-            attendance_service=attendance,
-        )
-
-        checkins = [c for c in checkins]
-
-        checkin_pks = [c.pk for c in checkins]
-
-        checkouts = Checkout.objects.filter(
-            checkin__in=checkin_pks
-        )
-
-        checkouts = [c for c in checkouts]
+        checkout = Checkout.objects.filter(
+            checkin__subscription=sub,
+            checkin__attendance_service=attendance,
+        ).order_by('registration', 'created_by').last()
 
         if row_idx not in collector:
             collector[row_idx] = []
@@ -424,29 +477,87 @@ def export_attendance_detailed_subscriptions(worksheet, subscriptions,
         collector[row_idx].append(get_object_value(sub, 'obs'))
         collector[row_idx].append(sub.created.strftime('%d/%m/%Y %H:%M:%S'))
 
-        checkin_str = ''
-        for i in range(1, len(checkins)):
-            checkin_str += "Checkin {}º feito ás {} \n".format(
-                i,
-                checkins[i - 1].created_on.strftime('%H:%M:%S de %d/%m/%Y '))
+        instance = checkout
 
-        if checkin_str == '':
-            checkin_str = '----'
+        if instance.registration:
+            t = instance.registration.strftime('%d/%m/%Y %H:%M:%S')
+        else:
+            t = instance.created_on.strftime('%d/%m/%Y %H:%M:%S')
 
-        collector[row_idx].append(checkin_str)
-
-        checkout_str = ''
-        for i in range(1, len(checkouts)):
-            checkout_str += "Checkout {}º feito ás {} \n".format(
-                i,
-                checkouts[i - 1].created_on.strftime('%H:%M:%S de %d/%m/%Y '))
-
-        if checkout_str == '':
-            checkout_str = '----'
-
-        collector[row_idx].append(checkout_str)
+        collector[row_idx].append("{}".format(t))
 
         row_idx += 1
 
     for row in collector.keys():
         worksheet.append(collector[row])
+
+
+def export_attendance_auditing(worksheet, subscriptions, attendance):
+    """ Exporta dados de inscrição. """
+
+    worksheet.append([
+        'CÓDIGO DA INSCRIÇÃO',
+        'NOME',
+        'CHECKIN',
+        'CHECKOUT',
+    ])
+
+    collector = {}
+    row_idx = 1
+    for sub in subscriptions:
+
+        checkins = Checkin.objects.filter(
+            subscription=sub,
+            attendance_service=attendance,
+        ).order_by('registration', 'created_by')
+
+        for checkin in checkins:
+
+            if row_idx not in collector:
+                collector[row_idx] = []
+
+            person = get_object_value(
+                obj=sub,
+                attr='person',
+                cached_type='subscriptions',
+            )
+
+            collector[row_idx].append(get_object_value(
+                obj=sub,
+                attr='code',
+                cached_type='subscriptions',
+            ))
+
+            collector[row_idx].append(get_object_value(person, 'name'))
+
+            instance = checkin
+
+            if instance.registration:
+                t = instance.registration.strftime('%d/%m/%Y %H:%M:%S')
+            else:
+                t = instance.created_on.strftime('%d/%m/%Y %H:%M:%S')
+
+            collector[row_idx].append("{}".format(t))
+
+            try:
+                checkout = Checkout.objects.get(checkin=checkin)
+
+                if instance.registration:
+                    t = checkout.registration.strftime('%d/%m/%Y %H:%M:%S')
+                else:
+                    t = checkout.created_on.strftime('%d/%m/%Y %H:%M:%S')
+
+                collector[row_idx].append('{}'.format(t))
+            except Checkout.DoesNotExist:
+                collector[row_idx].append('---')
+
+            row_idx += 1
+
+    for row in collector.keys():
+        worksheet.append(collector[row])
+
+
+def sort_subscriptions(subscriptions: list) -> QuerySet:
+    return Subscription.objects.filter(
+        uuid__in=[str(sub.pk) for sub in subscriptions]).order_by(
+        'person__name')
