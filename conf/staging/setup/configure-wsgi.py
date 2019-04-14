1a074b7f6950c76838cbe26b9c6dc434b768ef44
Hugo Seabra <hugoseabra19@gmail.com>
2019-04-14 14:12:35 -0300

    Correção de arquivo de configuração.

--- conf/staging/setup/configure-wsgi.py
+++ conf/staging/setup/configure-wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(sys.path.append(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from scripts import setup
 
