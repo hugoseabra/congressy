b6be6cbc38c4db4f0306557e7314a0fa96b6bd36
Nathan Martins <nathan.eua@gmail.com>
2019-05-29 15:58:50 -0300

    Correção de URL

--- ticket/urls.py
+++ ticket/urls.py
@@ -8,7 +8,7 @@ router.register(r'ticket/tickets', viewsets.TicketViewSet, base_name="ticket", )
 router.register(r'ticket/lots', viewsets.LotViewSet, base_name="lot")
 
 single_endpoints = [
-    url(r'^ticket/lots/(?P<event_pk>[\d]+)/calculator/(?P<price>\d+\.\d{2})',
+    url(r'^ticket/event/(?P<event_pk>[\d]+)/calculator/(?P<price>\d+\.\d+)',
         viewsets.TicketCalculatorAPIView.as_view(),
         name='ticket-calculator'),
     url(r'^ticket/tickets/(?P<pk>[\d]+)/lots/current/',
