a97d2e279e2e8bd7565821e46b00a298b46c8421
Hugo Seabra <hugoseabra19@gmail.com>
2019-07-10 02:16:36 -0300

    Removendo módulos payment e payment_debt

--- payment/models/__init__.py
+++ payment/models/__init__.py
@@ -1,4 +1,3 @@
 from .bank_account import BankAccount
 from .transaction import Transaction
-from .payment import Payment
 from .transaction_status import TransactionStatus
--- payment_debt/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaymentDebtConfig(AppConfig):
-    name = 'payment_debt'
-    verbose_name='Pendência'
-    verbose_name_plural='Pendênciass'
