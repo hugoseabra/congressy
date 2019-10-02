0d70bd9892954d734ab45b4eec8b63e15bac957d
Hugo Seabra <hugoseabra19@gmail.com>
2019-10-02 12:48:31 -0300

    Payment: viewset de benfeitor (pagador de uma pessoa) com melhorias.

--- payment/viewsets/benefactor.py
+++ payment/viewsets/benefactor.py
@@ -1,33 +1,16 @@
-from rest_framework import permissions, status
-from rest_framework.authentication import (
-    BasicAuthentication,
-    SessionAuthentication,
-    TokenAuthentication,
-)
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from gatheros_event.models import Person
 from payment.models import Benefactor
 from payment.serializers import (
     BenefactorSerializer,
 )
-
-
-class RestrictionViewMixin(object):
-    authentication_classes = (
-        SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-    )
-    permission_classes = (permissions.IsAuthenticated,)
+from .mixins import RestrictionViewMixin
 
 
 class BenefactorViewSet(RestrictionViewMixin, ModelViewSet):
     serializer_class = BenefactorSerializer
 
     def get_queryset(self):
-
         queryset = Benefactor.objects.get_queryset()
 
         user = self.request.user
