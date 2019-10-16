7f9b62937e5c26199c848deac7c74731ff0babe5
Hugo Seabra <hugoseabra19@gmail.com>
2019-10-16 14:48:24 -0300

    Payment: benefactor com novo schema de dados de endpoint REST

--- payment/serializers/benefactor.py
+++ payment/serializers/benefactor.py
@@ -10,3 +10,35 @@ class BenefactorSerializer(FormSerializerMixin, serializers.ModelSerializer):
         form = BenefactorManager
         model = Benefactor
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+
+        if instance.city_id:
+            city = instance.city
+
+            rep['city_data'] = {
+                'pk': instance.city_id,
+                'name': city.name,
+                'uf': city.uf,
+            }
+
+        person = instance.beneficiary
+        rep['beneficiary_data'] = {
+            'pk': person.pk,
+            'name': person.name,
+            'email': person.email,
+            'user': person.user_id,
+        }
+
+        if instance.beneficiary.user_id:
+            user = instance.beneficiary.user
+            rep['beneficiary_data']['user_data'] = {
+                'pk': user.pk,
+                'fist_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email,
+                'last_login': user.last_login,
+            }
+
+        return rep
