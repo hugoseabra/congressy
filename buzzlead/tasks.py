1ce77198753290b327f698bf18e4eb97a7772dae
Hugo Seabra <hugoseabra19@gmail.com>
2019-10-17 17:53:18 -0300

    Hotsite: Organizando banner de indicação.

--- buzzlead/tasks.py
+++ buzzlead/tasks.py
@@ -1,9 +1,13 @@
 from buzzlead.api_helper import create_bonus
+from buzzlead.sdk.base.exceptions import BuzzLeadException
 
 
 def confirm_bonus(token: str, email_campaign_owner: str, order_id: str):
     bonus = create_bonus(token=token,
                          email_campaign_owner=email_campaign_owner,
                          order_id=order_id)
+    try:
+        bonus.confirm()
+    except BuzzLeadException:
+        pass
 
-    bonus.confirm()
--- buzzlead/worker.py
+++ buzzlead/worker.py
@@ -2,13 +2,13 @@
 Task to send e-mails
 """
 
-from . import tasks
+from buzzlead import tasks
 from buzzlead.celery import app
 
 
 @app.task(bind=True,
           rate_limit='10/m',  # Processar até 10 tasks por minuto
-          default_retry_delay=30,  # retry in 30s
+          # default_retry_delay=30,  # retry in 30s
           ignore_result=True)
 def confirm_bonus(self, token: str, email_campaign_owner: str, order_id: str):
     try:
