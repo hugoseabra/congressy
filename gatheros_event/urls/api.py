cd0ca61ad839018ae52b2efb01092bc1b7e21a36
Hugo Seabra <hugoseabra19@gmail.com>
2019-10-30 15:41:33 -0300

    Event: endpoint de visualização de dados de pessoa do usuário logado e melhorias de segurança.

--- gatheros_event/urls/api.py
+++ gatheros_event/urls/api.py
@@ -7,6 +7,9 @@ router = routers.DefaultRouter()
 router.register(r'persons',
                 viewsets.PersonViewSet,
                 base_name="person",)
+router.register(r'person/me',
+                viewsets.PersonLoggedUserViewSet,
+                base_name="person-me",)
 
 router.register(r'event/organizations',
                 viewsets.OrganizationReadOnlyViewSet,
