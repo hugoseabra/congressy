8f4321a50379c8e45fa0fa207a6fb1369ea85128
Hugo Seabra <hugoseabra19@gmail.com>
2019-10-31 16:05:03 -0300

    Benefactor: serializer não cria mais de um benfeitor par auma pessoa se alguns dados forem os mesmos.

--- payment/serializers/benefactor.py
+++ payment/serializers/benefactor.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from rest_framework.fields import empty
 
 from core.serializers import FormSerializerMixin
+from gatheros_event.locale import locales
 from payment.managers import BenefactorManager
 from payment.models import Benefactor
 
@@ -10,6 +12,40 @@ class BenefactorSerializer(FormSerializerMixin, serializers.ModelSerializer):
         form = BenefactorManager
         model = Benefactor
         fields = '__all__'
+
+    def run_validation(self, data=empty):
+
+        if data and isinstance(data, dict):
+            is_br = data.get('country') == locales.BRASIL['codes']['digits_2']
+            is_company = data.get('is_company', False)
+
+            filter_search = {
+                'beneficiary_id': data.get('beneficiary')
+            }
+
+            if is_company is False:
+                if is_br:
+                    filter_search['cpf'] = data.get('cpf')
+
+                else:
+                    filter_search['doc_type'] = data.get('doc_type')
+                    filter_search['doc_number'] = \
+                        data.get('doc_number').strip()
+            else:
+                if is_br:
+                    filter_search['cnpj'] = data.get('cnpj')
+
+                else:
+                    filter_search['doc_type'] = data.get('doc_type')
+                    filter_search['doc_number'] = \
+                        data.get('doc_number').strip()
+
+            try:
+                self.instance = self.Meta.model.objects.get(**filter_search)
+            except self.Meta.model.DoesNotExist:
+                pass
+
+        return super().run_validation(data)
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
