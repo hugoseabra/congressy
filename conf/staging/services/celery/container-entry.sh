84ea265b6c477f4044c85f97ed9a109d47bd2eab
Hugo Seabra <hugoseabra19@gmail.com>
2019-11-18 17:39:39 -0300

    Correção: deploy de staging com entrypoint de celery errado

---
+++ conf/staging/services/celery/container-entry.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source /deploy/scripts.sh
+
+# Define settings
+export DJANGO_SETTINGS_MODULE=project.manage.settings.staging
+
+run_python_script "Configurando SETTINGS" /deploy/setup/configure-settings.py
+run_python_script_with_output "Atualizando Site ID" "manage.py loaddata 000_site"
+
+echo " > Iniciando CELERY"
+echo ;
+echo "########################################################################"
+echo ;
+celery \
+    -E --loglevel=INFO \
+    -A attendance \
+    -A mailer \
+    -A gatheros_subscription worker
