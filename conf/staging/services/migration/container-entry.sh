da5d0a6b1b663da3de91c43fc784104768dc34a4
Hugo Seabra <hugoseabra19@gmail.com>
2019-11-26 14:41:05 -0300

    Removendo reconstrução de DB no deploy de staging provisoriamente.

--- conf/staging/services/migration/container-entry.sh
+++ conf/staging/services/migration/container-entry.sh
@@ -10,7 +10,7 @@ run_python_script "Configurando SETTINGS" /deploy/setup/configure-settings.py
 run_python_script_with_output "Executando migrate" "manage.py migrate"
 run_python_script_with_output "Criado tabelas de cache" "manage.py createcachetable"
 run_python_script_with_output "Refazendo senha do admin padrão: hugo@congressy.com / mudar123@" "manage.py loaddata 001_admin_staging"
-run_python_script_with_output "Recriando token de acesso de teste" "manage.py loaddata 001_authtoken_staing"
+run_python_script_with_output "Recriando token de acesso de teste" "manage.py loaddata 001_authtoken_staging"
 
 # ADICIONA SUPER USUÁRIOS ÀS ORGANIZAÇÕES COM EVENTOS
 run_python_script_with_output "Adicionando admins à organizações" "manage.py admins_to_organizations"
