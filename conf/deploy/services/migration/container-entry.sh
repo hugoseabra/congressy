d329d5079ebcbda291808165efb42a598d6bc337
Hugo Seabra <hugoseabra19@gmail.com>
2020-01-30 19:03:53 -0300

    Melhoria em deploy.

--- conf/deploy/services/migration/container-entry.sh
+++ conf/deploy/services/migration/container-entry.sh
@@ -9,6 +9,7 @@ run_python_script "Configurando SETTINGS" /deploy/setup/configure-settings.py
 
 run_python_script_with_output "Executando migrate" "manage.py migrate"
 run_python_script_with_output "Criado tabelas de cache" "manage.py createcachetable"
+run_python_script_with_output "Atualizando Site ID" "manage.py loaddata 000_site"
 
 # ADICIONA SUPER USUÁRIOS ÀS ORGANIZAÇÕES COM EVENTOS
 run_python_script_with_output "Adicionando admins à organizações" "manage.py admins_to_organizations"
--- conf/staging/services/migration/container-entry.sh
+++ conf/staging/services/migration/container-entry.sh
@@ -9,6 +9,7 @@ run_python_script "Configurando SETTINGS" /deploy/setup/configure-settings.py
 
 run_python_script_with_output "Executando migrate" "manage.py migrate"
 run_python_script_with_output "Criado tabelas de cache" "manage.py createcachetable"
+run_python_script_with_output "Atualizando Site ID" "manage.py loaddata 000_site_staging"
 run_python_script_with_output "Refazendo senha do admin padrão: hugo@congressy.com / mudar123@" "manage.py loaddata 001_admin_staging"
 run_python_script_with_output "Recriando token de acesso de teste" "manage.py loaddata 001_authtoken_staging"
 
