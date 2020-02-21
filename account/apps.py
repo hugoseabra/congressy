5285a8417e612e9f63955b418f7bbeea2512daaf
Hugo Seabra <hugoseabra19@gmail.com>
2020-02-21 17:23:40 -0300

    Account: signal que exclui o token quando usuário realiza logout

--- account/apps.py
+++ account/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     name = 'account'
     verbose_name = 'Conta'
+
+    # noinspection PyUnresolvedReferences
+    def ready(self):
+        import account.signals
