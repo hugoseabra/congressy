c9f5dc0783a95f2c672860ad4908c97ce49c2e16
Hugo Seabra <hugoseabra19@gmail.com>
2020-03-06 10:35:26 -0300

    Melhorando configuração de filas do celery para todos os apps

--- gatheros_subscription/celery.py
+++ gatheros_subscription/celery.py
@@ -1,19 +1,31 @@
 import logging
+import os
 
 from celery import Celery
 from kombu import Queue, Exchange
 
+from project.system import get_system_alias
+from django.apps import apps
+
+dir_name = os.path.basename(os.path.dirname(__file__))
+
+system_alias = get_system_alias()
+namespace = 'CELERY_{}'.format(system_alias.upper())
+
+app = apps.get_app_config(app_label=dir_name)
+queue_name = app.name
+
 CELERY_QUEUES = (
     Queue(
-        'celery',
-        Exchange('transient', delivery_mode=2),
-        routing_key='celery',
+        name=system_alias,
+        exchange=Exchange(name='transient', type='direct', delivery_mode=2),
+        routing_key='{}.{}'.format(system_alias, queue_name),
         durable=True,
     ),
 )
 
-logger = logging.getLogger("Celery[SubscriptionExporter]")
+logger = logging.getLogger("Celery[{}]".format(queue_name.title()))
 
-app = Celery('subscription_exporter')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery(queue_name)
+app.config_from_object('django.conf:settings', namespace=namespace)
 app.autodiscover_tasks()
